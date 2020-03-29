0c2e9a46d1272e3daf989a115a383dafd0269817
Psikoi <ruben.amendoeira@gmail.com>
2020-03-29 02:24:56 +0100

    Migrate to Postgres and fix redis configs

--- server/src/api/jobs/redis.js
+++ server/src/api/jobs/redis.js
@@ -1,4 +1,8 @@
 module.exports = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false
+  }
 };
--- server/src/database/config.js
+++ server/src/database/config.js
@@ -3,15 +3,10 @@ require("dotenv").config({ path: process.env.NODE_ENV === "test" ? ".env.test" :
 module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "mysql",
+  dialect: process.env.DB_DIALECT,
   storage: process.env.DB_STORAGE,
   logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 3000,
-    idle: 10000
-  }
+  pool: { max: 5, min: 0, acquire: 3000, idle: 10000 }
 };
