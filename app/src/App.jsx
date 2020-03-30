b7201c69511049a950fd320c9460afcb4d216602
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-03-30 15:00:40 +0100

    Test proxy

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,15 @@ import NavigationBar from './components/NavigationBar';
 import Notification from './components/Notification';
 import { ROUTES } from './config/routing';
 
+import axios from 'axios';
+
+axios.get("http://server:5000/api").then(() => console.log("worked 1")).catch(e => console.log("failed 1"));
+axios.get("http://localhost:5000/api").then(() => console.log("worked 2")).catch(e => console.log("failed 2"));
+axios.get("http://localhost:3000/api").then(() => console.log("worked 3")).catch(e => console.log("failed 3"));
+axios.get("http://localhost/api").then(() => console.log("worked 4")).catch(e => console.log("failed 4"));
+axios.get("http://0.0.0.0/api").then(() => console.log("worked 5")).catch(e => console.log("failed 5"));
+axios.get("http://0.0.0.0:5000/api").then(() => console.log("worked 6")).catch(e => console.log("failed 6"));
+
 function App() {
   return (
     <BrowserRouter>
