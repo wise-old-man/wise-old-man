2b2a2ebbe5b570cf971c3f42c5c30106a83aa980
Psikoi <ruben.amendoeira@gmail.com>
2020-04-05 21:14:44 +0100

    Attempt to remove sqlite from git

--- server/src/api/jobs/index.js
+++ server/src/api/jobs/index.js
@@ -3,58 +3,62 @@ const redisConfig = require('./redis');
 
 const jobs = require('./instances');
 
-const queues = Object.values(jobs).map(job => ({
-  bull: new Queue(job.key, redisConfig),
-  name: job.key,
-  handle: job.handle,
-  onFail: job.onFail,
-  onSuccess: job.onSuccess
-}));
+function setup() {
+  const queues = Object.values(jobs).map(job => ({
+    bull: new Queue(job.key, redisConfig),
+    name: job.key,
+    handle: job.handle,
+    onFail: job.onFail,
+    onSuccess: job.onSuccess
+  }));
 
-// Supports cronjobs { repeat: { cron: "* * * * *" } }
-function add(name, data, options) {
-  const queue = queues.find(q => q.name === name);
+  // Supports cronjobs { repeat: { cron: "* * * * *" } }
+  function add(name, data, options) {
+    const queue = queues.find(q => q.name === name);
 
-  if (!queue) {
-    throw new Error(`No job found for name ${name}`);
+    if (!queue) {
+      throw new Error(`No job found for name ${name}`);
+    }
+
+    return queue.bull.add(data, options);
   }
 
-  return queue.bull.add(data, options);
-}
+  function schedule(name, data, date) {
+    const secondsTill = date - new Date();
 
-function schedule(name, data, date) {
-  const secondsTill = date - new Date();
-
-  // Don't allow scheduling for past dates
-  if (secondsTill >= 0) {
-    add(name, data, { delay: secondsTill });
+    // Don't allow scheduling for past dates
+    if (secondsTill >= 0) {
+      add(name, data, { delay: secondsTill });
+    }
   }
+
+  function process() {
+    return queues.forEach(queue => {
+      queue.bull.process(queue.handle);
+
+      queue.bull.on('completed', job => {
+        if (queue.onSuccess) {
+          queue.onSuccess(job.data);
+        }
+      });
+
+      queue.bull.on('failed', (job, err) => {
+        if (queue.onFail) {
+          queue.onFail(job.data, err);
+        } else {
+          console.log(`Job failed`, queue.name, job.data);
+          console.log(err);
+        }
+      });
+    });
+  }
+
+  return {
+    queues,
+    add,
+    process,
+    schedule
+  };
 }
 
-function process() {
-  return queues.forEach(queue => {
-    queue.bull.process(queue.handle);
-
-    queue.bull.on('completed', job => {
-      if (queue.onSuccess) {
-        queue.onSuccess(job.data);
-      }
-    });
-
-    queue.bull.on('failed', (job, err) => {
-      if (queue.onFail) {
-        queue.onFail(job.data, err);
-      } else {
-        console.log(`Job failed`, queue.name, job.data);
-        console.log(err);
-      }
-    });
-  });
-}
-
-module.exports = {
-  queues,
-  add,
-  process,
-  schedule
-};
+module.exports = process.env.NODE_ENV === 'test' ? { add: () => {} } : setup();
