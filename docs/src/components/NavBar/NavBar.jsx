06b1bc1c494d75fe2e412d0d4ac6a5a020c2798b
Psikoi <ruben.amendoeira@gmail.com>
2020-04-18 06:44:49 +0100

    Add homepage link to brand in docs nav bar

--- docs/src/components/NavBar/NavBar.jsx
+++ docs/src/components/NavBar/NavBar.jsx
@@ -5,14 +5,19 @@ function NavBar() {
   return (
     <div className="navigation-bar">
       <div className="navigation-bar__wrapper">
-        <div className="nav-brand">
-          <img className="nav-logo-img" src="/img/logo.png" alt="" />
-          <div className="nav-logo-text">
-            <span>WISE</span>
-            <span>OLD MAN</span>
+        <a href="/">
+          <div className="nav-brand">
+            <img className="nav-logo-img" src="/img/logo.png" alt="" />
+            <div className="nav-logo-text">
+              <span>WISE</span>
+              <span>OLD MAN</span>
+            </div>
           </div>
-        </div>
-        <a href="/">Return to the full site</a>
+        </a>
+
+        <a href="/">
+          <span className="link">Return to the full site</span>
+        </a>
       </div>
     </div>
   );
