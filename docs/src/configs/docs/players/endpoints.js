db0752bbdfb28a226c9c3c6513484f3dac837e86
Psikoi <ruben.amendoeira@gmail.com>
2020-04-29 21:05:25 +0100

    Add assert type endpoint documentation

--- docs/src/configs/docs/players/endpoints.js
+++ docs/src/configs/docs/players/endpoints.js
@@ -6,15 +6,15 @@ export default [
     comments: [
       {
         type: 'info',
-        content: 'This will only return the top 20 player results.',
-      },
+        content: 'This will only return the top 20 player results.'
+      }
     ],
     query: [
       {
         field: 'username',
         type: 'string',
-        description: 'A partial username match.',
-      },
+        description: 'A partial username match.'
+      }
     ],
     successResponses: [
       {
@@ -26,7 +26,7 @@ export default [
             type: 'ironman',
             lastImportedAt: null,
             registeredAt: '2020-04-04T21:48:19.197Z',
-            updatedAt: '2020-04-04T21:48:29.235Z',
+            updatedAt: '2020-04-04T21:48:29.235Z'
           },
           {
             id: 45,
@@ -34,19 +34,19 @@ export default [
             type: 'regular',
             lastImportedAt: null,
             registeredAt: '2020-04-04T21:48:31.367Z',
-            updatedAt: '2020-04-04T21:48:39.636Z',
-          },
-        ],
-      },
+            updatedAt: '2020-04-04T21:48:39.636Z'
+          }
+        ]
+      }
     ],
     errorResponses: [
       {
         description: 'If no username is given.',
         body: {
-          message: 'Invalid username.',
-        },
-      },
-    ],
+          message: 'Invalid username.'
+        }
+      }
+    ]
   },
   {
     title: 'View player',
@@ -55,20 +55,20 @@ export default [
     comments: [
       {
         type: 'warning',
-        content: 'If both "username" and "id" are given in the query params, "id" will be ignored.',
-      },
+        content: 'If both "username" and "id" are given in the query params, "id" will be ignored.'
+      }
     ],
     query: [
       {
         field: 'id',
         type: 'integer',
-        description: "The player's id.",
+        description: "The player's id."
       },
       {
         field: 'username',
         type: 'string',
-        description: "The player's username.",
-      },
+        description: "The player's username."
+      }
     ],
     successResponses: [
       {
@@ -85,36 +85,36 @@ export default [
             importedAt: null,
             overall: {
               rank: 30400,
-              experience: 269828205,
+              experience: 269828205
             },
             attack: {
               rank: 12158,
-              experience: 27216011,
-            },
-          },
-        },
-      },
+              experience: 27216011
+            }
+          }
+        }
+      }
     ],
     errorResponses: [
       {
         description: 'If no id or username are given.',
         body: {
-          message: 'Invalid player id.',
-        },
+          message: 'Invalid player id.'
+        }
       },
       {
         description: 'If an id is given but does not exist.',
         body: {
-          message: 'Player of id 5767 is not being tracked yet.',
-        },
+          message: 'Player of id 5767 is not being tracked yet.'
+        }
       },
       {
         description: 'If an username is given but does not exist.',
         body: {
-          message: 'someUsername is not being tracked yet.',
-        },
-      },
-    ],
+          message: 'someUsername is not being tracked yet.'
+        }
+      }
+    ]
   },
   {
     title: 'Track player',
@@ -125,17 +125,17 @@ export default [
         type: 'info',
         content:
           "If the player's type is unknown, this will trigger a \
-          job to determine the player's type.",
+          job to determine the player's type."
       },
       {
         type: 'info',
         content:
           "If the player hasn't been imported from CML within the \
-          last 24 hours, this will trigger a job to import the player's history from CML.",
-      },
+          last 24 hours, this will trigger a job to import the player's history from CML."
+      }
     ],
     body: {
-      username: 'psikoi',
+      username: 'psikoi'
     },
     successResponses: [
       {
@@ -152,37 +152,73 @@ export default [
             importedAt: null,
             overall: {
               rank: 30420,
-              experience: 269900478,
+              experience: 269900478
             },
             attack: {
               rank: 12159,
-              experience: 27216011,
-            },
-          },
-        },
-      },
+              experience: 27216011
+            }
+          }
+        }
+      }
     ],
     errorResponses: [
       {
         description: 'If no username is given.',
         body: {
-          message: 'Invalid username.',
-        },
+          message: 'Invalid username.'
+        }
       },
       {
         description: 'If the player was updated too recently (< 60 seconds)',
         body: {
-          message: 'Failed to update: psikoi was updated 43 seconds ago.',
-        },
+          message: 'Failed to update: psikoi was updated 43 seconds ago.'
+        }
       },
       {
         description:
           'If the username does not exist (in Runescape) OR failed to fetch from the hiscores.',
         body: {
-          message: 'Failed to load hiscores: Invalid username',
-        },
-      },
+          message: 'Failed to load hiscores: Invalid username'
+        }
+      }
+    ]
+  },
+  {
+    title: 'Assert player type (reasign)',
+    url: '/players/assert-type',
+    method: 'POST',
+    body: {
+      username: 'psikoi'
+    },
+    successResponses: [
+      {
+        description: '',
+        body: {
+          type: 'regular'
+        }
+      }
     ],
+    errorResponses: [
+      {
+        description: 'If no username is given.',
+        body: {
+          message: 'Invalid username.'
+        }
+      },
+      {
+        description: 'If the username is not tracked.',
+        body: {
+          message: 'Invalid player: Psikoi is not being tracked yet.'
+        }
+      },
+      {
+        description: 'If the API fails to fetch initial (regular) hiscores data.',
+        body: {
+          message: "Couldn't find player Psikoi in the hiscores."
+        }
+      }
+    ]
   },
   {
     title: 'Import player',
@@ -193,39 +229,39 @@ export default [
         type: 'info',
         content:
           "If the player's has been imported before, this will only import the CML history \
-          since the last import, otherwise it will import all the existing history.",
-      },
+          since the last import, otherwise it will import all the existing history."
+      }
     ],
     body: {
-      username: 'psikoi',
+      username: 'psikoi'
     },
     successResponses: [
       {
         description: '',
         body: {
-          message: '157 snapshots imported from CML.',
-        },
-      },
+          message: '157 snapshots imported from CML.'
+        }
+      }
     ],
     errorResponses: [
       {
         description: 'If no username is given.',
         body: {
-          message: 'Invalid username.',
-        },
+          message: 'Invalid username.'
+        }
       },
       {
         description: 'If the player was imported too recently (< 24 hours)',
         body: {
-          message: 'Imported too soon, please wait another 1354 minutes.',
-        },
+          message: 'Imported too soon, please wait another 1354 minutes.'
+        }
       },
       {
         description: 'If the username does not exist (in CML) OR failed to fetch from CML.',
         body: {
-          message: 'Failed to load history from CML.',
-        },
-      },
-    ],
-  },
+          message: 'Failed to load history from CML.'
+        }
+      }
+    ]
+  }
 ];
