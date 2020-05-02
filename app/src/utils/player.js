cdbc4af34201045fa85bddcbd9d9bcd8d20ece83
dekvall <dkvldev@gmail.com>
2020-05-02 20:09:31 +0200

    player: add link to osrs hiscores

--- app/src/utils/player.js
+++ app/src/utils/player.js
@@ -1,3 +1,25 @@
 export function getPlayerTypeIcon(type) {
   return `/img/runescape/player_types/${type}.png`;
 }
+
+export function getOfficialHiscoresUrl(player) {
+  const username = encodeURI(player.username);
+  let suffix;
+
+  switch (player.type) {
+    case 'ironman':
+      suffix = '_ironman';
+      break;
+    case 'ultimate':
+      suffix = '_ultimate';
+      break;
+    case 'hardcore':
+      suffix = '_hardcore_ironman';
+      break;
+    case 'regular':
+    default:
+      suffix = '';
+  }
+
+  return `https://secure.runescape.com/m=hiscore_oldschool${suffix}/hiscorepersonal.ws?user1=${username}`;
+}
