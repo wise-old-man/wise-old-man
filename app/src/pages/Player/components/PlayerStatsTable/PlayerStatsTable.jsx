b3a82a8d7248d876cb5ccf00d6df573c59346e3e
Knut Kirkhorn <knutkirk@hotmail.com>
2020-05-05 20:58:11 +0200

    Add virtual levels

--- app/src/pages/Player/components/PlayerStatsTable/PlayerStatsTable.jsx
+++ app/src/pages/Player/components/PlayerStatsTable/PlayerStatsTable.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import Table from '../../../../components/Table';
 import TableListPlaceholder from '../../../../components/TableListPlaceholder';
 import NumberLabel from '../../../../components/NumberLabel';
-import { capitalize, getSkillIcon, getLevel } from '../../../../utils';
+import { capitalize, getSkillIcon, getLevel, getVirtualLevel } from '../../../../utils';
 
 function PlayerStatsTable({ player, isLoading }) {
   if (isLoading) {
@@ -22,14 +22,29 @@ function PlayerStatsTable({ player, isLoading }) {
   const totalLevel = _.filter(filteredSnapshot, (val, key) => key !== 'overall')
     .map(skill => getLevel(skill.experience))
     .reduce((acc, cur) => acc + cur);
+  
+  const virtualTotalLevel = _.filter(filteredSnapshot, (val, key) => key !== 'overall')
+    .map(skill => getVirtualLevel(skill.experience))
+    .reduce((acc, cur) => acc + cur);
 
-  const rows = _.map(filteredSnapshot, (value, key) => ({
-    skill: key,
-    level: key === 'overall' ? totalLevel : getLevel(value.experience),
-    experience: value.experience,
-    rank: value.rank,
-    ehp: 0
-  }));
+  // Display virtual total level if there are any
+  const totalLevelString = virtualTotalLevel < totalLevel ? totalLevel : `${totalLevel} (${virtualTotalLevel})`;
+
+  const rows = _.map(filteredSnapshot, (value, key) => {
+    const currentLevel = getLevel(value.experience);
+    const currentVirtualLevel = getVirtualLevel(value.experience);
+
+    // Display the virtual level if above level 99
+    const currentLevelString = currentVirtualLevel < 100 ? currentLevel : `${currentLevel} (${currentVirtualLevel})`;
+
+    return {
+      skill: key,
+      level: key === 'overall' ? totalLevelString : currentLevelString,
+      experience: value.experience,
+      rank: value.rank,
+      ehp: 0
+    }
+  });
 
   // Column config
   const columns = [
