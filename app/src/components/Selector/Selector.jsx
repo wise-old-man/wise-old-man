afbbc1ab26c3d3e779a11428010ce15940f82496
Psikoi <ruben.amendoeira@gmail.com>
2020-05-08 00:18:40 +0100

    Rewrite Selector component

--- app/src/components/Selector/Selector.jsx
+++ app/src/components/Selector/Selector.jsx
@@ -1,74 +1,66 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Wrapper, Button, Menu, MenuItem } from 'react-aria-menubutton';
 import './Selector.scss';
 
-function Selector({ options, defaultOption, selectedIndex, onSelect, disabled }) {
-  const initialState = selectedIndex !== undefined ? options[selectedIndex] : defaultOption;
-  const [selectedOption, setSelectedOption] = useState(initialState || null);
+const itemClass = isSelected => classNames('selector-list__item', { '-selected': isSelected });
+const menuClass = isOpen => classNames('selector-list', { '-open': isOpen });
+const buttonClass = isDisabled => classNames({ 'selector-toggle': true, '-disabled': isDisabled });
 
-  const toggleOption = selectedOption || defaultOption || options[0];
-  const opts = defaultOption ? [defaultOption, ...options] : options;
+function Selector({ options, selectedIndex, onSelect, disabled }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleSelection = selectedValue => {
-    const option = options.find(o => o.value === selectedValue);
-    setSelectedOption(option);
-
+  function handleSelection(option) {
     if (onSelect) {
       onSelect(option);
     }
-  };
 
-  const onSelection = useCallback(handleSelection, [options, setSelectedOption, onSelect]);
+    close();
+  }
 
-  useEffect(() => {
-    if (selectedIndex && selectedIndex > -1) {
-      onSelection(opts[selectedIndex].value);
-    }
-  }, [onSelection, opts, selectedIndex]);
+  const onSelection = useCallback(handleSelection, [options, onSelect]);
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
 
-  const toggleClass = classNames({ selector__toggle: true, '-disabled': disabled });
+  const selectedOption = options && options[selectedIndex];
+
+  const label = selectedOption ? selectedOption.label : '---';
+  const icon = selectedOption && selectedOption.icon;
 
   return (
-    <Wrapper className="selector__container" onSelection={onSelection}>
-      <Button className={toggleClass}>
-        {toggleOption.icon && <img className="toggle__icon" src={toggleOption.icon} alt="" />}
-        <span className="toggle__text">{toggleOption.label}</span>
+    <div className="selector" onBlur={close}>
+      <button className={buttonClass(disabled)} type="button" onClick={open}>
+        {icon && <img className="toggle__icon" src={icon} alt="" />}
+        <span className="toggle__text">{disabled ? '' : label}</span>
         <img className="toggle__icon" src="/img/icons/dropdown_arrow_down.svg" alt="" />
-      </Button>
-      <Menu className="selector-list">
-        {opts.map(option => {
-          const itemClass = classNames({
-            'selector-list__item': true,
-            '-selected': selectedOption && option.label === selectedOption.label
-          });
-
-          return (
-            <MenuItem key={option.label} className={itemClass} value={option.value}>
-              {option.icon && <img className="selector-item__icon" src={option.icon} alt="" />}
-              <span className="selector-item__label">{option.label}</span>
-            </MenuItem>
-          );
-        })}
-      </Menu>
-    </Wrapper>
+      </button>
+      <div className={menuClass(isOpen)}>
+        {options &&
+          options.map((o, i) => (
+            <button
+              key={o.value}
+              type="button"
+              className={itemClass(i === selectedIndex)}
+              onMouseDown={() => onSelection(o)}
+            >
+              {o.label}
+            </button>
+          ))}
+      </div>
+    </div>
   );
 }
 
 Selector.defaultProps = {
-  defaultOption: undefined,
+  options: undefined,
   onSelect: undefined,
-  disabled: false,
-  selectedIndex: undefined
+  selectedIndex: undefined,
+  disabled: false
 };
 
 Selector.propTypes = {
   // A list of options, each of which should contain the fields (label, value) and optionally (icon)
-  options: PropTypes.arrayOf(PropTypes.shape).isRequired,
-
-  // The default option to display, usually useful for a "All" or "Any" option
-  defaultOption: PropTypes.shape(),
+  options: PropTypes.arrayOf(PropTypes.shape),
 
   // The selected index (controlled)
   selectedIndex: PropTypes.number,
