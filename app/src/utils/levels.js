04195b808214ecc809a71a21c168c857d633f78d
dekvall <dkvldev@gmail.com>
2020-05-08 17:48:30 +0200

    eslint: allow i++

--- app/src/utils/levels.js
+++ app/src/utils/levels.js
@@ -7,7 +7,7 @@ export function getLevel(experience, virtual = false) {
   }
   const maxlevel = virtual ? MAX_VIRTUAL_LEVEL : MAX_LEVEL;
   let accumulated = 0;
-  for (let level = 1; level < maxlevel; level += 1) {
+  for (let level = 1; level < maxlevel; level++) {
     const required = getXpDifferenceTo(level + 1);
     if (experience >= accumulated && experience < accumulated + required) {
       return level;
@@ -19,14 +19,14 @@ export function getLevel(experience, virtual = false) {
 
 export function getExperienceAt(level) {
   let accumulated = 0;
-  for (let l = 1; l !== level; l += 1) {
+  for (let l = 1; l !== level; l++) {
     accumulated += getXpDifferenceTo(l);
   }
   return accumulated;
 }
 
 function getXpDifferenceTo(level) {
-  if (level < 1) {
+  if (level < 2) {
     return 0;
   }
   return Math.floor(level - 1 + 300 * 2 ** ((level - 1) / 7)) / 4;
