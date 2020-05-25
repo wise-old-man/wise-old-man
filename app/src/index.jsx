59d82f3dc5c1c7906caa10c64ec068140f0942f2
Matias <34321338+breeku@users.noreply.github.com>
2020-05-25 17:05:19 +0300

    fix css load order (#153)
    * fixes css load order
    
    * add spaces

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import store from './redux/store';
-import App from './App';
 
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import './index.scss';
+
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
