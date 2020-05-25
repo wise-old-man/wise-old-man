b736c7abe8024bd3a9bd2b298d181802fd1ba3b6
breeku <bregfi@hotmail.com>
2020-05-25 16:44:32 +0300

    add spaces

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import './index.scss';
+
 import App from './App';
 
 ReactDOM.render(
