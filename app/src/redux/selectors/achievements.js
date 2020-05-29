1394012741ccb4ff39dbd43d8bd0e201289e099a
Psikoi <ruben.amendoeira@gmail.com>
2020-05-29 22:25:40 +0100

    Group achievements by metric and measure

--- app/src/redux/selectors/achievements.js
+++ app/src/redux/selectors/achievements.js
@@ -11,6 +11,35 @@ export const getAchievements = createSelector(achievementsSelector, map => Objec
 
 export const getPlayerAchievements = (state, playerId) => getAchievementsMap(state)[playerId];
 
+export const getPlayerAchievementsGrouped = (state, playerId) => {
+  const list = getPlayerAchievements(state, playerId);
+
+  if (!list) {
+    return [];
+  }
+
+  const sorted = list.sort((a, b) => {
+    return a.metric.localeCompare(b.metric) || a.measure.localeCompare(b.measure);
+  });
+
+  const groups = [];
+  let previousMetric = '';
+  let previousMeasure = '';
+
+  sorted.forEach(s => {
+    if (s.metric === previousMetric && s.measure === previousMeasure) {
+      groups[groups.length - 1].push(s);
+    } else {
+      groups.push([s]);
+    }
+
+    previousMetric = s.metric;
+    previousMeasure = s.measure;
+  });
+
+  return groups;
+};
+
 function formatAchievement(a) {
   const isDateUnknown = a.createdAt && a.createdAt.getFullYear() < 2000;
   return { ...a, unknownDate: !!isDateUnknown };
