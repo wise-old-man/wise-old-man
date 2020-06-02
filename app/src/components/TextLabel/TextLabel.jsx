fe095b63b50bd5225d2dfdc97c1da989d120db0f
ode <hello@ode.moe>
2020-06-01 21:08:25 -0400

    Add TextLabel component to render text on tables

---
+++ app/src/components/TextLabel/TextLabel.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './TextLabel.scss';
+
+
+function TextLabel({ value, popupValue }) {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const popupText = popupValue || value;
+  
+  const togglePopup = e => {
+    e.stopPropagation();
+    setIsPopupVisible(!isPopupVisible);
+  };
+
+  const hidePopup = () => setIsPopupVisible(false);
+
+  return (
+    <button className="text-label-btn" type="button" onClick={togglePopup} onBlur={hidePopup}>
+      {isPopupVisible && <div className="text-label-popup">{popupText}</div>}
+      <abbr className="text-label" title={popupText}>
+        <span>{value}</span>
+      </abbr>
+    </button>
+  );
+}
+
+TextLabel.defaultProps = {
+  popupValue: undefined
+};
+
+TextLabel.propTypes = {
+  value: PropTypes.string.isRequired,
+  popupValue: PropTypes.string
+};
+
+export default TextLabel;
