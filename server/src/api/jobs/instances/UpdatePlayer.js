751ce3ab21ee8e1c8282d858b94b0b5f31aec8da
Psikoi <ruben.amendoeira@gmail.com>
2020-06-20 23:07:27 +0100

    Add debugging logs to group update all

--- server/src/api/jobs/instances/UpdatePlayer.js
+++ server/src/api/jobs/instances/UpdatePlayer.js
@@ -4,6 +4,13 @@ module.exports = {
   name: 'UpdatePlayer',
   async handle({ data }) {
     const { username } = data;
+    console.log('Executing UpdatePlayer job', username);
     await playerService.update(username);
+  },
+  onFail(jobData, error) {
+    console.log('Failed UpdatePlayer job', jobData.username, error);
+  },
+  onSuccess(jobData) {
+    console.log('Completed UpdatePlayer job', jobData.username);
   }
 };
