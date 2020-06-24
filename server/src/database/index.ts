f427fad9ec7f1719733e9fbea9eb6ae84edf5f12
Jacob Keith <jacobakeith.cs@gmail.com>
2020-06-23 22:55:38 -0500

    chore: forgot this

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
 import {
   Achievement,
@@ -11,6 +12,7 @@ import {
   Snapshot
 } from './models';
 import config from './config';
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
 const sequelize = new Sequelize({
   host: config.host,
