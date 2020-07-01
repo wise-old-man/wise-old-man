4db8c2095e19afdaeb64e9d14f15063feae9c889
Psikoi <ruben.amendoeira@gmail.com>
2020-07-01 20:05:54 +0100

    Staging test 5

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' :
 
 const api = require('./api');
 
+// Staging test 5
+
 api.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
