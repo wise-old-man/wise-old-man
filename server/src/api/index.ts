7638819dcd713c4b5c34fe940a5e230855767f53
Psikoi <ruben.amendoeira@gmail.com>
2020-07-13 10:28:55 +0100

    Refactor /api/index.ts to class object

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,32 +1,54 @@
-import { isTesting } from '../env';
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 import rateLimit from 'express-rate-limit';
-import { router } from './routing';
-import jobs from './jobs';
+import { isTesting } from '../env';
 import * as hooks from './hooks';
+import jobs from './jobs';
 import * as proxies from './proxies';
+import { router } from './routing';
 
 const RATE_LIMIT_MINUTES = 5;
 const RATE_LIMIT_REQUESTS = 500;
 
-const api = express();
+class API {
+  express;
 
-api.use(express.json());
-api.use(express.urlencoded({ extended: true }));
-api.use(cors());
+  constructor() {
+    this.express = express();
 
-if (!isTesting()) {
-  api.set('trust proxy', 1);
+    this.setupMiddlewares();
+    this.setupRouting();
 
-  // Limits 500 requests per ip, every 5 minutes
-  api.use(rateLimit({ windowMs: RATE_LIMIT_MINUTES * 60 * 1000, max: RATE_LIMIT_REQUESTS }));
+    if (!isTesting()) {
+      this.setupServices();
+    }
+  }
 
-  jobs.setup();
-  hooks.setup();
-  proxies.setup();
+  setupMiddlewares() {
+    this.express.set('trust proxy', 1);
+
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
+    this.express.use(cors());
+
+    // Limits 500 requests per ip, every 5 minutes
+    this.express.use(
+      rateLimit({
+        windowMs: RATE_LIMIT_MINUTES * 60 * 1000,
+        max: RATE_LIMIT_REQUESTS
+      })
+    );
+  }
+
+  setupRouting() {
+    this.express.use('/api', router);
+  }
+
+  setupServices() {
+    jobs.setup();
+    hooks.setup();
+    proxies.setup();
+  }
 }
 
-api.use('/api', router);
-
-export default api;
+export default new API().express;
