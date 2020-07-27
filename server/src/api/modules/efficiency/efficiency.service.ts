cc484687e1b802ea64ab19110c56f5ebd1ae37d6
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-07-27 21:35:50 +0100

    Add base EHP/EHB algorithms (#356)

---
+++ server/src/api/modules/efficiency/efficiency.service.ts
@@ -0,0 +1,30 @@
+import { BOSSES, SKILLS } from '../../constants';
+import { getValueKey } from '../../util/metrics';
+import Snapshot from '../snapshots/snapshot.model';
+import mainAlgorithm from './algorithms/main';
+
+function calculateEHP(snapshot: Snapshot): number {
+  // TODO: always use main ehp, for now
+  const algorithm = mainAlgorithm;
+  const exp = Object.fromEntries(SKILLS.map(s => [s, snapshot[getValueKey(s)]]));
+
+  return algorithm.calculateEHP(exp);
+}
+
+function calculateEHB(snapshot: Snapshot) {
+  // TODO: always use main ehp, for now
+  const algorithm = mainAlgorithm;
+  const kcs = Object.fromEntries(BOSSES.map(b => [b, snapshot[getValueKey(b)]]));
+
+  return algorithm.calculateEHB(kcs);
+}
+
+function calculateEHPDiff(beforeSnapshot: Snapshot, afterSnapshot: Snapshot): number {
+  return calculateEHP(afterSnapshot) - calculateEHP(beforeSnapshot);
+}
+
+function calculateEHBDiff(beforeSnapshot: Snapshot, afterSnapshot: Snapshot): number {
+  return calculateEHB(afterSnapshot) - calculateEHB(beforeSnapshot);
+}
+
+export { calculateEHP, calculateEHB, calculateEHBDiff, calculateEHPDiff };
