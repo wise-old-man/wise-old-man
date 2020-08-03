6578a3e2e01180f0812892c3fe04b88daba495b4
Louis Dion-Marcil <louisdionmarcil@gmail.com>
2020-08-02 23:48:00 -0400

    Added HiscoreNotFoundError for when player is not in Hiscores

--- server/src/api/errors/index.ts
+++ server/src/api/errors/index.ts
@@ -1,5 +1,6 @@
 import BadRequestError from './instances/BadRequestError';
 import NotFoundError from './instances/NotFoundError';
+import HiscoreNotFoundError from './instances/HiscoreNotFoundError';
 import ServerError from './instances/ServerError';
 
-export { BadRequestError, ServerError, NotFoundError };
+export { BadRequestError, ServerError, NotFoundError, HiscoreNotFoundError };
