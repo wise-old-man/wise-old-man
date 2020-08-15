6a128c8e033abc89f739da0fa14a99c72bba6bad
Psikoi <ruben.amendoeira@gmail.com>
2020-08-15 17:56:15 +0100

    Improve error message codes on competition service

--- server/src/api/errors/index.ts
+++ server/src/api/errors/index.ts
@@ -1,6 +1,7 @@
 import BadRequestError from './instances/BadRequestError';
+import ForbiddenError from './instances/ForbiddenError';
 import NotFoundError from './instances/NotFoundError';
 import RateLimitError from './instances/RateLimitError';
 import ServerError from './instances/ServerError';
 
-export { BadRequestError, ServerError, NotFoundError, RateLimitError };
+export { BadRequestError, ServerError, NotFoundError, RateLimitError, ForbiddenError };
