4cca6d394f24a2bb53fe1ac5b00655c272ac17de
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-10-27 23:01:20 +0000

    Test trailblazer deployment triggers 4 (server)

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import env from './env';
 
 const port = env.PORT || 5000;
 
-// Test API deployment trigger 3
+// Test API deployment trigger 4
 
 api.listen(port, () => {
   console.log(`Server running on port ${port}`);
