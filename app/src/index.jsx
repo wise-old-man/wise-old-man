f94a138aa4359776f92752edded5c28ccadd3153
Psikoi <ruben.amendoeira@gmail.com>
2020-10-27 21:50:44 +0000

    Test trailblazer deployment triggers

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -8,6 +8,8 @@ import './index.scss';
 
 import App from './App';
 
+// Test APP deployment trigger
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import api from './api';
 
 const port = env.PORT || 5000;
 
+// Test API deployment trigger
+
 api.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
