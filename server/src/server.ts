43b2016433bc7a272d51b7a4ed423de7c8b5741e
Psikoi <ruben.amendoeira@gmail.com>
2020-10-28 11:49:40 +0000

    Enable league tracking

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,7 @@
-import api from './api';
 import env from './env';
+import api from './api';
 
 const port = env.PORT || 6000;
-
-// Test API deployment trigger 4
 
 api.listen(port, () => {
   console.log(`Server running on port ${port}`);
