4d1e9bf0535a902d1e86601af3b459d821e65763
Psikoi <ruben.amendoeira@gmail.com>
2020-12-07 07:41:54 +0000

    Refactor leaderboards page

---
+++ app/src/pages/Leaderboards/containers/Controls.jsx
@@ -0,0 +1,76 @@
+import React, { useContext } from 'react';
+import { Selector } from 'components';
+import { PLAYER_TYPES, PLAYER_BUILDS, VIRTUALS } from 'config';
+import { capitalize, getPlayerIcon, getPlayerBuild, getMetricIcon, getMetricName } from 'utils';
+import { LeaderboardContext } from '../context';
+
+const PLAYER_TYPES_OPTIONS = PLAYER_TYPES.map(type => ({
+  label: capitalize(type),
+  icon: getPlayerIcon(type),
+  value: type
+}));
+
+const PLAYER_BUILDS_OPTIONS = [
+  { label: 'All player builds', value: null },
+  ...PLAYER_BUILDS.map(type => ({
+    label: getPlayerBuild(type),
+    value: type
+  }))
+];
+
+const METRIC_OPTIONS = [...VIRTUALS, 'ehp+ehb'].map(metric => ({
+  label: getMetricName(metric),
+  icon: getMetricIcon(metric, true),
+  value: metric
+}));
+
+function Controls() {
+  const { context, updateContext } = useContext(LeaderboardContext);
+  const { metric, type, build } = context;
+
+  const metricIndex = METRIC_OPTIONS.findIndex(o => o.value === metric);
+  const playerTypeIndex = PLAYER_TYPES_OPTIONS.findIndex(o => o.value === type);
+  const playerBuildIndex = PLAYER_BUILDS_OPTIONS.findIndex(o => o.value === build);
+
+  const handleMetricSelected = e => {
+    if (!e || !e.value) return;
+    updateContext({ metric: e.value });
+  };
+
+  const handleTypeSelected = e => {
+    updateContext({ type: e.value });
+  };
+
+  const handleBuildSelected = e => {
+    updateContext({ build: e.value });
+  };
+
+  return (
+    <>
+      <div className="col-lg-4 col-md-3">
+        <Selector
+          options={METRIC_OPTIONS}
+          selectedIndex={metricIndex}
+          onSelect={handleMetricSelected}
+          search
+        />
+      </div>
+      <div className="col-lg-4 col-md-5">
+        <Selector
+          options={PLAYER_TYPES_OPTIONS}
+          selectedIndex={playerTypeIndex}
+          onSelect={handleTypeSelected}
+        />
+      </div>
+      <div className="col-lg-4 col-md-5">
+        <Selector
+          options={PLAYER_BUILDS_OPTIONS}
+          selectedIndex={playerBuildIndex}
+          onSelect={handleBuildSelected}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Controls;
---
+++ app/src/pages/Leaderboards/containers/List.jsx
@@ -0,0 +1,66 @@
+import React, { useMemo, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { leaderboardsSelectors } from 'redux/leaderboards';
+import { Table, PlayerTag, TablePlaceholder } from 'components';
+import { durationBetween, formatNumber } from 'utils';
+import { LeaderboardContext } from '../context';
+
+function List() {
+  const { context } = useContext(LeaderboardContext);
+  const { metric } = context;
+
+  const tableConfig = useMemo(() => getTableConfig(metric), [metric]);
+
+  // Memoized redux variables
+  const leaderboards = useSelector(leaderboardsSelectors.getLeaderboards);
+  const isLoading = useSelector(leaderboardsSelectors.isFetching);
+
+  if (isLoading && (!leaderboards || leaderboards.length === 0)) {
+    return <TablePlaceholder size={20} />;
+  }
+
+  return (
+    <Table
+      uniqueKeySelector={tableConfig.uniqueKey}
+      columns={tableConfig.columns}
+      rows={leaderboards}
+      listStyle
+    />
+  );
+}
+
+function getTableConfig(metric) {
+  return {
+    uniqueKey: row => row.id,
+    columns: [
+      {
+        key: 'rank',
+        label: 'Rank',
+        className: () => '-break-small'
+      },
+      {
+        key: 'displayName',
+        label: 'Name',
+        className: () => '-primary',
+        transform: (value, row) => (
+          <Link to={`/players/${row.username}`}>
+            <PlayerTag name={row.displayName} type={row.type} flagged={row.flagged} />
+          </Link>
+        )
+      },
+      {
+        key: metric,
+        transform: val => `${formatNumber(val)} hours`
+      },
+      {
+        key: 'updatedAt',
+        label: 'Last updated',
+        className: () => '-break-small',
+        transform: (value, row) => `Updated ${durationBetween(row.updatedAt, new Date(), 2, true)} ago`
+      }
+    ]
+  };
+}
+
+export default List;
