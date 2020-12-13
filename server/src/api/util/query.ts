7126c2a5f81aa0ba6fbbf4be16087fba6b479913
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-12-13 21:48:27 +0000

    Fix buildQuery function for empty strings (#541)

--- server/src/api/util/query.ts
+++ server/src/api/util/query.ts
@@ -2,7 +2,11 @@ export function buildQuery(params: any): any {
   const query: any = {};
 
   Object.keys(params).forEach(k => {
-    if (params[k] !== undefined && params[k] !== null) {
+    if (
+      params[k] !== undefined &&
+      params[k] !== null &&
+      (typeof params[k] !== 'string' || params[k].length > 0)
+    ) {
       query[k] = params[k];
     }
   });
