0373545e6d2bde94b71b43ae32cedf700a74bc99
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-12-13 19:02:51 +0000

    Fix name change status query (#539)

--- server/src/api/util/query.ts
+++ server/src/api/util/query.ts
@@ -2,7 +2,7 @@ export function buildQuery(params: any): any {
   const query: any = {};
 
   Object.keys(params).forEach(k => {
-    if (params[k]) {
+    if (params[k] !== undefined && params[k] !== null) {
       query[k] = params[k];
     }
   });
