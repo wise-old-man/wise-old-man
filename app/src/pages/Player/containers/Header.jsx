c7c64e6dc442fd7e16bba3ed4768209ab75dbb97
Psikoi <ruben.amendoeira@gmail.com>
2021-01-23 03:13:25 +0000

    Add country flag to player profile header

--- app/src/pages/Player/containers/Header.jsx
+++ app/src/pages/Player/containers/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { getPlayerTypeIcon, getPlayerTooltip, getOfficialHiscoresUrl } from 'utils';
-import { PageHeader, Dropdown, Button } from 'components';
+import { PageHeader, Dropdown, Button, Badge } from 'components';
 
 const MENU_OPTIONS = [
   { label: 'Open official hiscores', value: 'OPEN_HISCORES' },
@@ -33,15 +33,30 @@ function Header(props) {
         title={player.displayName}
         icon={getPlayerTypeIcon(player.type)}
         iconTooltip={getPlayerTooltip(player.type, player.flagged)}
-        badges={getPlayerBadges(player.build)}
-      >
-        <Button text="Update" onClick={handleUpdate} loading={isTracking} />
-        <Dropdown options={MENU_OPTIONS} onSelect={handleOptionSelected}>
-          <button className="header__options-btn" type="button">
-            <img src="/img/icons/options.svg" alt="" />
-          </button>
-        </Dropdown>
-      </PageHeader>
+        renderLeft={() => {
+          const buildBadge = getBuildBadge(player.build);
+          return (
+            <>
+              {buildBadge && <Badge text={buildBadge.text} hoverText={buildBadge.hoverText} />}
+              {player.country && (
+                <abbr className="flag" title={`Country: ${player.country}`}>
+                  <img src={`/img/flags/${player.country}.svg`} alt={player.country} />
+                </abbr>
+              )}
+            </>
+          );
+        }}
+        renderRight={() => (
+          <>
+            <Button text="Update" onClick={handleUpdate} loading={isTracking} />
+            <Dropdown options={MENU_OPTIONS} onSelect={handleOptionSelected}>
+              <button className="header__options-btn" type="button">
+                <img src="/img/icons/options.svg" alt="" />
+              </button>
+            </Dropdown>
+          </>
+        )}
+      />
     </>
   );
 }
@@ -73,18 +88,18 @@ function FlaggedWarning({ displayName }) {
   );
 }
 
-function getPlayerBadges(build) {
+function getBuildBadge(build) {
   switch (build) {
     case 'lvl3':
-      return [{ text: 'Level 3', hoverText: '' }];
+      return { text: 'Level 3', hoverText: '' };
     case 'f2p':
-      return [{ text: 'F2P', hoverText: '' }];
+      return { text: 'F2P', hoverText: '' };
     case '1def':
-      return [{ text: '1 Def Pure', hoverText: '' }];
+      return { text: '1 Def Pure', hoverText: '' };
     case '10hp':
-      return [{ text: '10 HP Pure', hoverText: '' }];
+      return { text: '10 HP Pure', hoverText: '' };
     default:
-      return [];
+      return null;
   }
 }
 
@@ -94,7 +109,8 @@ Header.propTypes = {
     displayName: PropTypes.string,
     flagged: PropTypes.bool,
     type: PropTypes.string,
-    build: PropTypes.string
+    build: PropTypes.string,
+    country: PropTypes.string
   }).isRequired,
   isTracking: PropTypes.bool.isRequired,
   handleUpdate: PropTypes.func.isRequired,
