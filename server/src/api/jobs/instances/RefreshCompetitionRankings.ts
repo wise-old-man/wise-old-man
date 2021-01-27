6c0c3fd525caf095ab1548aeaeeefdcf0785ff32
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2021-01-27 18:19:43 +0000

    Fix group/competition score calculations (#632)

--- server/src/api/jobs/instances/RefreshCompetitionRankings.ts
+++ server/src/api/jobs/instances/RefreshCompetitionRankings.ts
@@ -11,12 +11,12 @@ class RefreshCompetitionRankings implements Job {
   }
 
   async handle(): Promise<void> {
-    const allCompetitions = await competitionService.getList({}, { limit: 100_000, offset: 0 });
+    const allCompetitions = await Competition.findAll();
 
     await Promise.all(
       allCompetitions.map(async competition => {
         const currentScore = competition.score;
-        const newScore = await this.calculateScore(competition);
+        const newScore = await calculateScore(competition);
 
         if (newScore !== currentScore) {
           await competition.update({ score: newScore });
@@ -24,104 +24,104 @@ class RefreshCompetitionRankings implements Job {
       })
     );
   }
+}
 
-  async calculateScore(competition: Competition): Promise<number> {
-    const now = new Date();
-    let score = 0;
+async function calculateScore(competition: Competition): Promise<number> {
+  const now = new Date();
+  let score = 0;
 
-    // If has ended
-    if (competition.endsAt < now) {
-      return score;
-    }
+  // If has ended
+  if (competition.endsAt < now) {
+    return score;
+  }
 
-    const details = await competitionService.getDetails(competition);
-    const activeParticipants = details.participants.filter(p => p.progress.gained > 0);
-    const averageGained = details.totalGained / activeParticipants.length;
+  const details = await competitionService.getDetails(competition);
+  const activeParticipants = details.participants.filter(p => p.progress.gained > 0);
+  const averageGained = details.totalGained / activeParticipants.length;
 
-    // If is ongoing
-    if (competition.startsAt <= now && competition.endsAt >= now) {
-      score += 100;
-    }
+  // If is ongoing
+  if (competition.startsAt <= now && competition.endsAt >= now) {
+    score += 100;
+  }
 
-    // If is upcoming
-    if (competition.startsAt > now) {
-      const daysLeft = (competition.startsAt.getTime() - now.getTime()) / 1000 / 3600 / 24;
+  // If is upcoming
+  if (competition.startsAt > now) {
+    const daysLeft = (competition.startsAt.getTime() - now.getTime()) / 1000 / 3600 / 24;
 
-      if (daysLeft > 7) {
-        score += 60;
-      } else {
-        score += 80;
-      }
-    }
-
-    // If is group competition
-    if (details.group) {
-      // The highest of 30, or 30% of the group's score
-      score += Math.max(40, details.group.score * 0.4);
-
-      if (details.group.verified) {
-        score += 50;
-      }
-    }
-
-    // If has atleast 10 active participants
-    if (activeParticipants.length >= 10) {
+    if (daysLeft > 7) {
       score += 60;
-    }
-
-    // If has atleast 50 active participants
-    if (activeParticipants.length >= 50) {
+    } else {
       score += 80;
     }
+  }
 
-    if (isSkill(competition.metric)) {
-      // If the average active participant has gained > 10k exp
-      if (averageGained > 10000) {
-        score += 30;
-      }
+  // If is group competition
+  if (details.group) {
+    // The highest of 30, or 30% of the group's score
+    score += Math.max(40, details.group.score * 0.4);
 
-      // If the average active participant has gained > 100k exp
-      if (averageGained > 100000) {
-        score += 50;
-      }
+    if (details.group.verified) {
+      score += 50;
     }
+  }
 
-    if (isBoss(competition.metric)) {
-      // If the average active participant has gained > 5 kc
-      if (averageGained > 5) {
-        score += 30;
-      }
+  // If has atleast 10 active participants
+  if (activeParticipants.length >= 10) {
+    score += 60;
+  }
 
-      // If the average active participant has gained > 30 kc
-      if (averageGained > 20) {
-        score += 50;
-      }
-    }
+  // If has atleast 50 active participants
+  if (activeParticipants.length >= 50) {
+    score += 80;
+  }
 
-    if (isActivity(competition.metric)) {
-      // If the average active participant has gained > 5 score
-      if (averageGained > 5) {
-        score += 30;
-      }
-
-      // If the average active participant has gained > 50 score
-      if (averageGained > 20) {
-        score += 50;
-      }
-    }
-
-    // Discourage "overall" competitions, they are often tests
-    if (competition.metric !== 'overall') {
+  if (isSkill(competition.metric)) {
+    // If the average active participant has gained > 10k exp
+    if (averageGained > 10000) {
       score += 30;
     }
 
-    // Discourage "over 2 weeks long" competitions
-    if (competition.endsAt.getTime() - competition.startsAt.getTime() < 1209600000) {
+    // If the average active participant has gained > 100k exp
+    if (averageGained > 100000) {
       score += 50;
     }
-
-    return score;
   }
+
+  if (isBoss(competition.metric)) {
+    // If the average active participant has gained > 5 kc
+    if (averageGained > 5) {
+      score += 30;
+    }
+
+    // If the average active participant has gained > 30 kc
+    if (averageGained > 20) {
+      score += 50;
+    }
+  }
+
+  if (isActivity(competition.metric)) {
+    // If the average active participant has gained > 5 score
+    if (averageGained > 5) {
+      score += 30;
+    }
+
+    // If the average active participant has gained > 50 score
+    if (averageGained > 20) {
+      score += 50;
+    }
+  }
+
+  // Discourage "overall" competitions, they are often tests
+  if (competition.metric !== 'overall') {
+    score += 30;
+  }
+
+  // Discourage "over 2 weeks long" competitions
+  if (competition.endsAt.getTime() - competition.startsAt.getTime() < 1209600000) {
+    score += 50;
+  }
+
+  return score;
 }
 
 export default new RefreshCompetitionRankings();
--- server/src/api/jobs/instances/RefreshGroupRankings.ts
+++ server/src/api/jobs/instances/RefreshGroupRankings.ts
@@ -16,7 +16,7 @@ class RefreshGroupRankings implements Job {
     await Promise.all(
       allGroups.map(async group => {
         const currentScore = group.score;
-        const newScore = await this.calculateScore(group);
+        const newScore = await calculateScore(group);
 
         if (newScore !== currentScore) {
           await group.update({ score: newScore });
@@ -24,77 +24,77 @@ class RefreshGroupRankings implements Job {
       })
     );
   }
+}
 
-  async calculateScore(group: Group): Promise<number> {
-    let score = 0;
+async function calculateScore(group: Group): Promise<number> {
+  let score = 0;
 
-    const now = new Date();
-    const members = await groupService.getMembersList(group);
-    const pagination = { limit: 100, offset: 0 };
-    const competitions = await competitionService.getGroupCompetitions(group.id, pagination);
-    const averageOverallExp = members.reduce((acc: any, cur: any) => acc + cur, 0) / members.length;
+  const now = new Date();
+  const members = await groupService.getMembersList(group);
+  const pagination = { limit: 100, offset: 0 };
+  const competitions = await competitionService.getGroupCompetitions(group.id, pagination);
+  const averageOverallExp = members.reduce((acc: any, cur: any) => acc + cur, 0) / members.length;
 
-    if (!members || members.length === 0) {
-      return score;
-    }
-
-    // If has atleast one leader
-    if (members.filter(m => m.role === 'leader').length >= 1) {
-      score += 30;
-    }
-
-    // If has atleast 10 players
-    if (members.length >= 10) {
-      score += 20;
-    }
-
-    // If has atleast 50 players
-    if (members.length >= 50) {
-      score += 40;
-    }
-
-    // If average member overall exp > 30m
-    if (averageOverallExp >= 30_000_000) {
-      score += 30;
-    }
-
-    // If average member overall exp > 100m
-    if (averageOverallExp >= 100_000_000) {
-      score += 60;
-    }
-
-    // If has a clan chat
-    if (group.clanChat && group.clanChat.length > 0) {
-      score += 50;
-    }
-
-    // If has a description
-    if (group.description && group.description.length > 0) {
-      score += 40;
-    }
-
-    // If has a homeworld
-    if (group.homeworld && group.homeworld > 0) {
-      score += 20;
-    }
-
-    // If is verified (clan leader is in our discord)
-    if (group.verified) {
-      score += 100;
-    }
-
-    // If has atleast one ongoing competition
-    if (competitions.filter(c => c.startsAt <= now && c.endsAt >= now).length >= 1) {
-      score += 50;
-    }
-
-    // If has atleast one upcoming competition
-    if (competitions.filter(c => c.startsAt >= now).length >= 1) {
-      score += 30;
-    }
-
+  if (!members || members.length === 0) {
     return score;
   }
+
+  // If has atleast one leader
+  if (members.filter(m => m.role === 'leader').length >= 1) {
+    score += 30;
+  }
+
+  // If has atleast 10 players
+  if (members.length >= 10) {
+    score += 20;
+  }
+
+  // If has atleast 50 players
+  if (members.length >= 50) {
+    score += 40;
+  }
+
+  // If average member overall exp > 30m
+  if (averageOverallExp >= 30_000_000) {
+    score += 30;
+  }
+
+  // If average member overall exp > 100m
+  if (averageOverallExp >= 100_000_000) {
+    score += 60;
+  }
+
+  // If has a clan chat
+  if (group.clanChat && group.clanChat.length > 0) {
+    score += 50;
+  }
+
+  // If has a description
+  if (group.description && group.description.length > 0) {
+    score += 40;
+  }
+
+  // If has a homeworld
+  if (group.homeworld && group.homeworld > 0) {
+    score += 20;
+  }
+
+  // If is verified (clan leader is in our discord)
+  if (group.verified) {
+    score += 100;
+  }
+
+  // If has atleast one ongoing competition
+  if (competitions.filter(c => c.startsAt <= now && c.endsAt >= now).length >= 1) {
+    score += 50;
+  }
+
+  // If has atleast one upcoming competition
+  if (competitions.filter(c => c.startsAt >= now).length >= 1) {
+    score += 30;
+  }
+
+  return score;
 }
 
 export default new RefreshGroupRankings();
