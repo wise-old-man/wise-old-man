fc7654b4ccbda918865602d16ecb4328d9365286
Psikoi <ruben.amendoeira@gmail.com>
2021-03-14 15:19:03 +0000

    Fix participation table onDelete policy

--- server/src/database/migrations/20200826223818-add-startSnapshot-col-participation.ts
+++ server/src/database/migrations/20200826223818-add-startSnapshot-col-participation.ts
@@ -3,6 +3,7 @@ import { QueryInterface } from 'sequelize/types';
 function up(queryInterface: QueryInterface, dataTypes: any): Promise<void> {
   return queryInterface.addColumn('participations', 'startSnapshotId', {
     type: dataTypes.INTEGER,
+    onDelete: 'SET NULL',
     references: {
       model: 'snapshots',
       key: 'id'
