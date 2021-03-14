8f72c24462b8516beb2f9561acb7092c40e114a4
Psikoi <ruben.amendoeira@gmail.com>
2021-03-14 17:14:49 +0000

    Add job cache & cooldowns

--- server/src/api/jobs/index.ts
+++ server/src/api/jobs/index.ts
@@ -1,5 +1,5 @@
 import Queue from 'bull';
-import env, { isTesting } from '../../env';
+import { isDevelopment, isRunningInMainCPUCore, isTesting } from '../../env';
 import logger from '../services/external/logger.service';
 import crons from './config/crons';
 import redisConfig from './config/redis';
@@ -12,14 +12,22 @@ export interface Job {
   onFailure?(data: any, error: Error): void;
 }
 
-enum JobPriority {
-  High = 1,
-  Medium = 2,
-  Low = 3
+export enum JobPriority {
+  HIGH = 1,
+  MEDIUM = 2,
+  LOW = 3
+}
+
+export interface JobQueue extends Job {
+  bull: any;
+}
+export interface JobRunCache {
+  [key: string]: number;
 }
 
 class JobHandler {
-  private queues;
+  private queues: JobQueue[];
+  private cachedRuns: JobRunCache;
 
   constructor() {
     this.queues = jobs.map((job: Job) => ({
@@ -29,36 +37,41 @@ class JobHandler {
       onFailure: job.onFailure,
       onSuccess: job.onSuccess
     }));
+
+    this.cachedRuns = {};
   }
 
   /**
    * Adds a new job to the queue, to be executed ASAP.
    */
-  add(name, data, options?) {
-    if (isTesting()) {
-      return;
-    }
+  add(name: string, data: any, options?: any) {
+    if (isTesting()) return;
 
     const queue = this.queues.find(q => q.name === name);
 
-    if (!queue) {
-      throw new Error(`No job found for name ${name}`);
+    if (!queue) throw new Error(`No job found for name ${name}`);
+
+    if (options?.cache) {
+      const lastExecutionTime = this.cachedRuns[options.cache.identifier];
+
+      // Skipping execution, the cooldown time hasn't passed yet
+      if (lastExecutionTime && lastExecutionTime > Date.now() - options.cache.cooldown) return;
     }
 
-    const priority = (options && options.priority) || JobPriority.Medium;
+    const priority = (options && options.priority) || JobPriority.MEDIUM;
     queue.bull.add({ ...data, created: new Date() }, { ...options, priority });
   }
 
   /**
    * Adds new scheduled job, to be executed at the specified date.
    */
-  schedule(name, data, date) {
+  schedule(name: string, data: any, date: Date) {
     const msDiff = date.getTime() - Date.now();
 
     // Don't allow scheduling for past dates
-    if (msDiff >= 0) {
-      this.add(name, data, { delay: msDiff, priority: JobPriority.Medium });
-    }
+    if (msDiff < 0) return;
+
+    this.add(name, data, { delay: msDiff, priority: JobPriority.MEDIUM });
   }
 
   init() {
@@ -70,6 +83,11 @@ class JobHandler {
       queue.bull.on('completed', job => {
         if (queue.onSuccess) {
           queue.onSuccess(job.data);
+        }
+
+        // If this job has cache configs, update this run's "last executed" timestamp
+        if (job?.opts?.cache) {
+          this.cachedRuns[job.opts.cache.identifier] = Date.now();
         }
       });
 
@@ -88,8 +106,7 @@ class JobHandler {
 
     // If running through pm2 (production), only run cronjobs on the first CPU core.
     // Otherwise, on a 4 core server, every cronjob would run 4x as often.
-    // Note: This will also run in development environments
-    if (!env.pm_id || parseInt(env.pm_id, 10) === 0) {
+    if (isRunningInMainCPUCore() || isDevelopment()) {
       // Remove any active cron jobs
       this.queues.forEach(async ({ bull }) => {
         const activeQueues = await bull.getRepeatableJobs();
@@ -100,7 +117,7 @@ class JobHandler {
       // TODO: this can be improved to await for the removal above, instead of the hacky 10sec wait
       setTimeout(() => {
         crons.forEach(({ jobName, cronConfig }) =>
-          this.add(jobName, null, { repeat: { cron: cronConfig }, priority: JobPriority.Low })
+          this.add(jobName, null, { repeat: { cron: cronConfig }, priority: JobPriority.LOW })
         );
       }, 10000);
     }
