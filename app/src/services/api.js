d31cb3413bae64d36fd45069f3136f8733cb53f8
Psikoi <ruben.amendoeira@gmail.com>
2021-04-17 16:33:19 +0100

    Change App Base API Url

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api';
+export const BASE_URL = process.env.REACT_APP_BASE_API_URL || 'https://api.wiseoldman.net';
 
 const API = axios.create({
   baseURL: BASE_URL,
