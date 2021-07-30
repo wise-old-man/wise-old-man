1f846b31ef36cc031207cd386c049dc56a22541c
dekvall <dkvldev@gmail.com>
2021-07-30 17:24:36 +0200

    EHP & EHB rate update

--- server/src/api/modules/efficiency/configs/ehp/f2p.ehp.ts
+++ server/src/api/modules/efficiency/configs/ehp/f2p.ehp.ts
@@ -4,43 +4,43 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 7200,
-        description: 'Clan wars alt-killing'
+        rate: 7_200,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 37224,
-        rate: 14400,
-        description: 'Clan wars alt-killing'
+        startExp: 37_224,
+        rate: 17_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 100000,
-        rate: 28900,
-        description: 'Clan wars alt-killing'
+        startExp: 100_000,
+        rate: 34_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1000000,
-        rate: 43000,
-        description: 'Clan wars alt-killing'
+        startExp: 1_000_000,
+        rate: 50_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1986068,
-        rate: 51000,
-        description: 'Clan wars alt-killing'
+        startExp: 1_986_068,
+        rate: 60_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 3000000,
-        rate: 57800,
-        description: 'Clan wars alt-killing'
+        startExp: 3_000_000,
+        rate: 68_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 5346332,
-        rate: 63000,
-        description: 'Clan wars alt-killing'
+        startExp: 5_346_332,
+        rate: 72_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 13034431,
-        rate: 68000,
-        description: 'Clan wars alt-killing'
+        startExp: 13_034_431,
+        rate: 80_000,
+        description: 'Castle wars alt-killing'
       }
     ],
     bonuses: []
@@ -50,8 +50,8 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 68000,
-        description: 'Clan wars alt-killing'
+        rate: 80_000,
+        description: 'Castle wars alt-killing'
       }
     ],
     bonuses: []
@@ -61,43 +61,43 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 7200,
-        description: 'Clan wars alt-killing'
+        rate: 7_200,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 37224,
-        rate: 14400,
-        description: 'Clan wars alt-killing'
+        startExp: 37_224,
+        rate: 17_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 100000,
-        rate: 28900,
-        description: 'Clan wars alt-killing'
+        startExp: 100_000,
+        rate: 34_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1000000,
-        rate: 43000,
-        description: 'Clan wars alt-killing'
+        startExp: 1_000_000,
+        rate: 50_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1986068,
-        rate: 51000,
-        description: 'Clan wars alt-killing'
+        startExp: 1_986_068,
+        rate: 60_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 3000000,
-        rate: 57800,
-        description: 'Clan wars alt-killing'
+        startExp: 3_000_000,
+        rate: 68_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 5346332,
-        rate: 63000,
-        description: 'Clan wars alt-killing'
+        startExp: 5_346_332,
+        rate: 72_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 13034431,
-        rate: 68000,
-        description: 'Clan wars alt-killing'
+        startExp: 13_034_431,
+        rate: 80_000,
+        description: 'Castle wars alt-killing'
       }
     ],
     bonuses: []
@@ -108,7 +108,7 @@ export default [
       {
         startExp: 0,
         rate: 0,
-        description: 'Clan wars alt-killing'
+        description: 'Castle wars alt-killing'
       }
     ],
     bonuses: []
@@ -118,43 +118,43 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 7000,
-        description: 'Clan wars alt-killing'
+        rate: 7_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 37224,
-        rate: 14000,
-        description: 'Clan wars alt-killing'
+        startExp: 37_224,
+        rate: 17_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 100000,
-        rate: 28000,
-        description: 'Clan wars alt-killing'
+        startExp: 100_000,
+        rate: 34_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1000000,
-        rate: 42500,
-        description: 'Clan wars alt-killing'
+        startExp: 1_000_000,
+        rate: 50_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 1986068,
-        rate: 49000,
-        description: 'Clan wars alt-killing'
+        startExp: 1_986_068,
+        rate: 60_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 3000000,
-        rate: 56500,
-        description: 'Clan wars alt-killing'
+        startExp: 3_000_000,
+        rate: 68_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 5346332,
-        rate: 61200,
-        description: 'Clan wars alt-killing'
+        startExp: 5_346_332,
+        rate: 72_000,
+        description: 'Castle wars alt-killing'
       },
       {
-        startExp: 13034431,
-        rate: 66000,
-        description: 'Clan wars alt-killing'
+        startExp: 13_034_431,
+        rate: 80_000,
+        description: 'Castle wars alt-killing'
       }
     ],
     bonuses: []
@@ -164,7 +164,7 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 43000,
+        rate: 105_000,
         description: 'Bury big bones'
       }
     ],
@@ -186,23 +186,23 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 40000,
+        rate: 40_000,
         description: 'Best fish'
       },
       {
-        startExp: 7842,
-        rate: 130000,
+        startExp: 7_842,
+        rate: 130_000,
         description: 'Best fish'
       },
       {
-        startExp: 37224,
-        rate: 175000,
+        startExp: 37_224,
+        rate: 175_000,
         description: 'Wines'
       },
       {
-        startExp: 737627,
-        rate: 480000,
-        description: 'Wines'
+        startExp: 224_466,
+        rate: 2_052_000,
+        description: '3t fish-cook method'
       }
     ],
     bonuses: []
@@ -212,42 +212,42 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 7000,
+        rate: 7_000,
         description: 'Trees'
       },
       {
-        startExp: 2411,
-        rate: 15000,
+        startExp: 2_411,
+        rate: 15_000,
         description: '3t Oak trees'
       },
       {
-        startExp: 13363,
-        rate: 28000,
+        startExp: 13_363,
+        rate: 28_000,
         description: '2.66t, 2x snow Willows'
       },
       {
-        startExp: 41171,
-        rate: 45000,
+        startExp: 41_171,
+        rate: 45_000,
         description: '2.66t, 2x snow Willows'
       },
       {
-        startExp: 302288,
-        rate: 58500,
+        startExp: 302_288,
+        rate: 58_500,
         description: '2.66t, 2x snow Willows'
       },
       {
-        startExp: 1986068,
-        rate: 70000,
+        startExp: 1_986_068,
+        rate: 70_000,
         description: '2.66t, 2x snow Willows'
       },
       {
-        startExp: 5346332,
-        rate: 81000,
+        startExp: 5_346_332,
+        rate: 81_000,
         description: '2.66t, 2x snow Willows'
       },
       {
-        startExp: 13034431,
-        rate: 90000,
+        startExp: 13_034_431,
+        rate: 90_000,
         description: '2.66t, 2x snow Willows'
       }
     ],
@@ -269,43 +269,43 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 14000,
+        rate: 14_000,
         description: 'Best fish'
       },
       {
-        startExp: 4470,
-        rate: 26700,
-        description: '3t Snow fishing w/ leather'
+        startExp: 4_470,
+        rate: 28_000,
+        description: 'Best fish'
       },
       {
-        startExp: 13363,
-        rate: 33300,
-        description: '3t Snow fishing w/ leather'
+        startExp: 13_363,
+        rate: 35_250,
+        description: '3t fish-cook method'
       },
       {
-        startExp: 273742,
-        rate: 57000,
-        description: '3t Snow fishing w/ leather'
+        startExp: 273_742,
+        rate: 60_350,
+        description: '3t fish-cook method'
       },
       {
-        startExp: 737627,
-        rate: 67000,
-        description: '3t Snow fishing w/ leather'
+        startExp: 737_627,
+        rate: 71_000,
+        description: '3t fish-cook method'
       },
       {
-        startExp: 2500000,
-        rate: 72000,
-        description: '3t Snow fishing w/ leather'
+        startExp: 2_500_000,
+        rate: 76_250,
+        description: '3t fish-cook method'
       },
       {
-        startExp: 6000000,
-        rate: 76500,
-        description: '3t Snow fishing w/ leather'
+        startExp: 6_000_000,
+        rate: 81_000,
+        description: '3t fish-cook method'
       },
       {
-        startExp: 13034431,
-        rate: 85000,
-        description: '3t Snow fishing w/ leather'
+        startExp: 13_034_431,
+        rate: 90_000,
+        description: '3t fish-cook method'
       }
     ],
     bonuses: []
@@ -315,23 +315,23 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 45000,
-        description: 'Best logs @ GE w/ b bones'
+        rate: 45_000,
+        description: 'Best logs @ GE w/ vile ashes'
       },
       {
-        startExp: 13363,
-        rate: 130500,
-        description: 'Best logs @ GE w/ b bones'
+        startExp: 13_363,
+        rate: 130_500,
+        description: 'Best logs @ GE w/ vile ashes'
       },
       {
-        startExp: 61512,
-        rate: 195750,
-        description: 'Best logs @ GE w/ b bones'
+        startExp: 61_512,
+        rate: 195_750,
+        description: 'Best logs @ GE w/ vile ashes'
       },
       {
-        startExp: 273742,
-        rate: 293625,
-        description: 'Best logs @ GE w/ b bones'
+        startExp: 273_742,
+        rate: 293_625,
+        description: 'Best logs @ GE w/ vile ashes'
       }
     ],
     bonuses: [
@@ -340,24 +340,24 @@ export default [
         bonusSkill: 'prayer',
         startExp: 13_363,
         endExp: 61_512,
-        end: true,
-        ratio: 0.1666
+        end: false,
+        ratio: 0.2666
       },
       {
         originSkill: 'firemaking',
         bonusSkill: 'prayer',
         startExp: 61_512,
         endExp: 273_742,
-        end: true,
-        ratio: 0.1111
+        end: false,
+        ratio: 0.1777
       },
       {
         originSkill: 'firemaking',
         bonusSkill: 'prayer',
         startExp: 273_742,
         endExp: 200_000_000,
-        end: true,
-        ratio: 0.0741
+        end: false,
+        ratio: 0.125
       }
     ]
   },
@@ -366,17 +366,17 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 57000,
+        rate: 57_000,
         description: 'Best gems'
       },
       {
-        startExp: 4470,
-        rate: 135000,
+        startExp: 4_470,
+        rate: 135_000,
         description: 'Best gems'
       },
       {
-        startExp: 50339,
-        rate: 295000,
+        startExp: 50_339,
+        rate: 295_000,
         description: 'Best gems'
       }
     ],
@@ -387,12 +387,12 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 4385776,
+        rate: 4_385_776,
         description: "Knight's sword -> superheat"
       },
       {
-        startExp: 4385776,
-        rate: 275000,
+        startExp: 4_385_776,
+        rate: 275_000,
         description: 'Addy bodies w/ alt'
       }
     ],
@@ -403,27 +403,27 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 4000,
+        rate: 4_000,
         description: 'Tin/copper'
       },
       {
-        startExp: 14833,
-        rate: 12000,
+        startExp: 14_833,
+        rate: 12_000,
         description: '3t iron with alt'
       },
       {
-        startExp: 41171,
-        rate: 25000,
+        startExp: 41_171,
+        rate: 25_000,
         description: '3t iron with alt'
       },
       {
-        startExp: 111945,
-        rate: 57000,
+        startExp: 111_945,
+        rate: 57_000,
         description: '3t iron with alt'
       },
       {
-        startExp: 737627,
-        rate: 65000,
+        startExp: 737_627,
+        rate: 65_000,
         description: '3t iron with alt'
       }
     ],
@@ -498,7 +498,7 @@ export default [
     methods: [
       {
         startExp: 0,
-        rate: 55000,
+        rate: 55_000,
         description: 'Pvp world body tiaras'
       }
     ],
