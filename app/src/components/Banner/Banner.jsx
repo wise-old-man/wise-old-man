0169750e4c441b984bd6aa9e840b9cbb84704345
Huskiez <pokepin1@hotmail.com>
2022-02-01 04:34:23 +1000

    Fix leagues banner on mobile (#930)
    * add guide for creating database to server-guide
    
    * set max height for ParticipantSelector and edit scrollbar
    
    * revert ParticipantsSelector
    
    * add max-height and scrollbar to TeamComposeModal
    
    * fix mobile display for leagues banner + move nbsp to <p>

--- app/src/components/Banner/Banner.jsx
+++ app/src/components/Banner/Banner.jsx
@@ -4,8 +4,7 @@ import './Banner.scss';
 function Banner() {
   return (
     <div className="banner__container">
-      <p>Playing the Shattered Relics League? Check out our</p>
-      &nbsp;
+      <p>Playing the Shattered Relics League? Check out our &nbsp;</p>
       <a href="https://league.wiseoldman.net">"Wise Old Man - League Edition"</a>
     </div>
   );
