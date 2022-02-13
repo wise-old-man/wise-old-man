f1b2945826d046f8db4af867f14c2eb5ad584b7d
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2022-02-13 18:03:47 +0000

    Divide connection pool max/min by thread count (#934)

--- server/src/database/config.ts
+++ server/src/database/config.ts
@@ -1,5 +1,7 @@
 import env, { isTesting } from '../env';
 
+const CPU_COUNT = env.CPU_COUNT ? parseInt(env.CPU_COUNT) : 1;
+
 export = {
   database: 'wise-old-man',
   dialect: isTesting() ? 'sqlite' : 'postgres',
@@ -8,6 +10,13 @@ export = {
   password: env.POSTGRES_PASSWORD,
   storage: env.DB_STORAGE,
   logging: false,
-  pool: { max: 40, min: 5, acquire: 30000, idle: 10000 },
-  retry: { max: 5 }
+  pool: {
+    max: Math.round(40 / CPU_COUNT),
+    min: Math.round(5 / CPU_COUNT),
+    acquire: 30000,
+    idle: 10000
+  },
+  retry: {
+    max: 5
+  }
 };
