bff2385edd04b11f2b0e4689a89b9b0eb87982c3
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2022-08-13 15:34:14 +0200

    Fix database url (env var resolver) & add deploy/test workflows (#960)
    * Fix database url env var resolver
    
    * Restore v2 deploy workflow, and add v2 test workflow

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import env, { getThreadIndex } from './env';
 import api from './api';
 
-const port = env.API_PORT || 5000;
+const port = env.API_PORT || 5001;
 
 api.listen(port, () => {
-  console.log(`Server running on port ${port}. Thread Index: ${getThreadIndex()}`);
+  console.log(`v2: Server running on port ${port}. Thread Index: ${getThreadIndex()}`);
 });
