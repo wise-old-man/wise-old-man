f2722b38f81f7a25bbc25ee19448e18a7dc89500
Psikoi <ruben.amendoeira@gmail.com>
2023-04-26 11:40:19 +0200

    Ydfg

--- app-v2/src/app/leaderboards/[section]/page.tsx
+++ app-v2/src/app/leaderboards/[section]/page.tsx
@@ -38,21 +38,23 @@ export default async function LeaderboardsPage(props: LeaderboardsPageProps) {
     playerBuild: getPlayerBuildParam(searchParams.playerBuild),
   };
 
-  return (
-    <>
-      {/* @ts-expect-error - Server Component  */}
-      <Leaderboard period={Period.DAY} filters={filters} />
-      {/* Wrap these in suspense to allow the UI to be shown as soon as day leaderboards are loaded */}
-      <Suspense fallback={<LeaderboardSkeleton period={Period.WEEK} />}>
-        {/* @ts-expect-error - Server Component  */}
-        <Leaderboard period={Period.WEEK} filters={filters} />
-      </Suspense>
-      <Suspense fallback={<LeaderboardSkeleton period={Period.MONTH} />}>
-        {/* @ts-expect-error - Server Component  */}
-        <Leaderboard period={Period.MONTH} filters={filters} />
-      </Suspense>
-    </>
-  );
+  // return (
+  //   <>
+  //     {/* @ts-expect-error - Server Component  */}
+  //     <Leaderboard period={Period.DAY} filters={filters} />
+  //     {/* Wrap these in suspense to allow the UI to be shown as soon as day leaderboards are loaded */}
+  //     <Suspense fallback={<LeaderboardSkeleton period={Period.WEEK} />}>
+  //       {/* @ts-expect-error - Server Component  */}
+  //       <Leaderboard period={Period.WEEK} filters={filters} />
+  //     </Suspense>
+  //     <Suspense fallback={<LeaderboardSkeleton period={Period.MONTH} />}>
+  //       {/* @ts-expect-error - Server Component  */}
+  //       <Leaderboard period={Period.MONTH} filters={filters} />
+  //     </Suspense>
+  //   </>
+  // );
+
+  return <div>content!</div>;
 }
 
 interface LeaderboardProps {
