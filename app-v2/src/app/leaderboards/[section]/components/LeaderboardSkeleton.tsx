db9630101aeb861438cfb362e89670a946919dad
Psikoi <ruben.amendoeira@gmail.com>
2023-05-03 14:10:01 +0200

    Improve records loading state

--- app-v2/src/app/leaderboards/[section]/components/LeaderboardSkeleton.tsx
+++ app-v2/src/app/leaderboards/[section]/components/LeaderboardSkeleton.tsx
@@ -3,23 +3,27 @@ import { ListTable, ListTableRow, ListTableCell } from "~/components/ListTable";
 
 interface LeaderboardSkeletonProps {
   period: Period;
+  hasCaption?: boolean;
 }
 
 export function LeaderboardSkeleton(props: LeaderboardSkeletonProps) {
-  const { period } = props;
+  const { period, hasCaption } = props;
 
   return (
     <div>
       <h3 className="pb-3 text-h3 font-bold">{PeriodProps[period].name}</h3>
       <ListTable>
-        {[...Array(20)].map((i) => (
+        {[...Array(20)].map((_, i) => (
           <ListTableRow key={`${period}_skeleton_${i}`}>
             <ListTableCell className="w-1 pr-1">
               <div className="h-4 w-4 animate-pulse rounded-xl bg-gray-600" />
             </ListTableCell>
             <ListTableCell className="flex items-center text-sm text-white">
               <div className="h-8 w-8 shrink-0 animate-pulse rounded-full bg-gray-600" />
-              <div className="ml-2 h-4 w-24 animate-pulse rounded-xl bg-gray-600" />
+              <div className="ml-2 flex flex-col gap-y-1.5">
+                <div className="h-4 w-24 animate-pulse rounded-xl bg-gray-600" />
+                {hasCaption && <div className="h-3.5 w-32 animate-pulse rounded-xl bg-gray-600" />}
+              </div>
             </ListTableCell>
             <ListTableCell className="w-5 text-right font-medium">
               <div className="h-5 w-12 animate-pulse rounded-xl bg-gray-600" />
--- app-v2/src/app/leaderboards/[section]/loading.tsx
+++ app-v2/src/app/leaderboards/[section]/loading.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { Period } from "@wise-old-man/utils";
+import { usePathname } from "next/navigation";
 import { LeaderboardSkeleton } from "./components/LeaderboardSkeleton";
 
 export default function Loading() {
+  const pathname = usePathname();
+  const isRecordsLoading = pathname.includes("records");
+
   return (
     <>
-      <LeaderboardSkeleton period={Period.DAY} />
-      <LeaderboardSkeleton period={Period.WEEK} />
-      <LeaderboardSkeleton period={Period.MONTH} />
+      <LeaderboardSkeleton period={Period.DAY} hasCaption={isRecordsLoading} />
+      <LeaderboardSkeleton period={Period.WEEK} hasCaption={isRecordsLoading} />
+      <LeaderboardSkeleton period={Period.MONTH} hasCaption={isRecordsLoading} />
     </>
   );
 }
