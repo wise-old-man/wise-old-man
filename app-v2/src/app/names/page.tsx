c448a8dca089f1d6f696b71422308bac5e0321d1
Psikoi <ruben.amendoeira@gmail.com>
2023-05-06 15:38:17 +0200

    Add pagination

--- app-v2/src/app/names/page.tsx
+++ app-v2/src/app/names/page.tsx
@@ -1,16 +1,20 @@
 import { NameChange, NameChangeStatus } from "@wise-old-man/utils";
 import { Badge } from "~/components/Badge";
+import { Pagination } from "~/components/Pagination";
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/Tooltip";
 import { ListTable, ListTableCell, ListTableRow } from "~/components/ListTable";
 import { apiClient } from "~/utils/api";
 import { timeago } from "~/utils/dates";
-import { getNameChangeStatusParam, getSearchParam } from "~/utils/params";
+import { getNameChangeStatusParam, getPageParam, getSearchParam } from "~/utils/params";
 import { capitalize } from "~/utils/strings";
 
 import ArrowRightIcon from "~/assets/arrow_right.svg";
 
+const RESULTS_PER_PAGE = 20;
+
 interface PageProps {
   searchParams: {
+    page?: string;
     modal?: string;
     search?: string;
     status?: string;
@@ -24,66 +28,73 @@ export function generateMetadata(props: PageProps) {
     return { title: "Submit New Name Change" };
   }
 
+  const page = getPageParam(searchParams.page) || 1;
   const status = getNameChangeStatusParam(searchParams.status);
 
   if (status) {
-    return { title: `(${capitalize(status)}) Name Changes` };
+    return { title: `(${capitalize(status)}) Name Changes (Page ${page})` };
   }
 
-  return { title: "Name Changes" };
+  return { title: `Name Changes (Page ${page})` };
 }
 
 export default async function NameChangesPage(props: PageProps) {
   const { searchParams } = props;
 
+  const page = getPageParam(searchParams.page) || 1;
   const search = getSearchParam(searchParams.search);
   const status = getNameChangeStatusParam(searchParams.status);
 
-  const data = await apiClient.nameChanges.searchNameChanges({
-    username: search,
-    status,
-  });
-
-  if (!data || data.length === 0) {
-    return (
-      <div className="w-full rounded border border-gray-700 py-10 text-center text-sm text-gray-300">
-        No results were found
-      </div>
-    );
-  }
+  const data = await apiClient.nameChanges.searchNameChanges(
+    { username: search, status },
+    { limit: RESULTS_PER_PAGE, offset: page * RESULTS_PER_PAGE }
+  );
 
   return (
-    <ListTable>
-      {data.map((nameChange) => (
-        <ListTableRow key={nameChange.id}>
-          <ListTableCell>{nameChange.id}</ListTableCell>
-          <ListTableCell className="py-4 text-sm font-medium text-white">
-            {nameChange.oldName}
-          </ListTableCell>
-          <ListTableCell>
-            <ArrowRightIcon className="h-4 w-4 text-white" />
-          </ListTableCell>
-          <ListTableCell className="text-sm font-medium text-white">{nameChange.newName}</ListTableCell>
-          <ListTableCell>
-            <Tooltip>
-              <TooltipTrigger>Submitted {timeago.format(nameChange.createdAt)}</TooltipTrigger>
-              <TooltipContent>{formatDate(nameChange.createdAt)}</TooltipContent>
-            </Tooltip>
-          </ListTableCell>
-          <ListTableCell>
-            {nameChange.resolvedAt && (
-              <Tooltip>
-                <TooltipTrigger>{getResolvedTimeago(nameChange)}</TooltipTrigger>
-                <TooltipContent>{formatDate(nameChange.resolvedAt)}</TooltipContent>
-              </Tooltip>
-            )}
-          </ListTableCell>
-          <ListTableCell>
-            <StatusBadge status={nameChange.status} />
-          </ListTableCell>
-        </ListTableRow>
-      ))}
-    </ListTable>
+    <>
+      {!data || data.length === 0 ? (
+        <div className="w-full rounded border border-gray-700 py-10 text-center text-sm text-gray-300">
+          No results were found
+        </div>
+      ) : (
+        <ListTable>
+          {data.map((nameChange) => (
+            <ListTableRow key={nameChange.id}>
+              <ListTableCell>{nameChange.id}</ListTableCell>
+              <ListTableCell className="py-4 text-sm font-medium text-white">
+                {nameChange.oldName}
+              </ListTableCell>
+              <ListTableCell>
+                <ArrowRightIcon className="h-4 w-4 text-white" />
+              </ListTableCell>
+              <ListTableCell className="text-sm font-medium text-white">
+                {nameChange.newName}
+              </ListTableCell>
+              <ListTableCell>
+                <Tooltip>
+                  <TooltipTrigger>Submitted {timeago.format(nameChange.createdAt)}</TooltipTrigger>
+                  <TooltipContent>{formatDate(nameChange.createdAt)}</TooltipContent>
+                </Tooltip>
+              </ListTableCell>
+              <ListTableCell>
+                {nameChange.resolvedAt && (
+                  <Tooltip>
+                    <TooltipTrigger>{getResolvedTimeago(nameChange)}</TooltipTrigger>
+                    <TooltipContent>{formatDate(nameChange.resolvedAt)}</TooltipContent>
+                  </Tooltip>
+                )}
+              </ListTableCell>
+              <ListTableCell>
+                <StatusBadge status={nameChange.status} />
+              </ListTableCell>
+            </ListTableRow>
+          ))}
+        </ListTable>
+      )}
+      <div className="mt-2">
+        <Pagination currentPage={page} hasMorePages={data.length >= RESULTS_PER_PAGE} />
+      </div>
+    </>
   );
 }
 
--- app-v2/src/utils/params.ts
+++ app-v2/src/utils/params.ts
@@ -8,43 +8,44 @@ import {
 } from "@wise-old-man/utils";
 
 export function getMetricParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!isMetric(param)) return undefined;
+  if (!param || !isMetric(param)) return undefined;
   return param;
 }
 
 export function getPlayerTypeParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!isPlayerType(param)) return undefined;
+  if (!param || !isPlayerType(param)) return undefined;
   return param;
 }
 
 export function getPlayerBuildParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!isPlayerBuild(param)) return undefined;
+  if (!param || !isPlayerBuild(param)) return undefined;
   return param;
 }
 
 export function getCountryParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!isCountry(param)) return undefined;
+  if (!param || !isCountry(param)) return undefined;
   return param;
 }
 
 export function getComputedMetricParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!isComputedMetric(param) && param !== "combined") return undefined;
+  if (!param || (!isComputedMetric(param) && param !== "combined")) return undefined;
   return param;
 }
 
 export function getNameChangeStatusParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (!Object.values(NameChangeStatus).includes(param as any)) return undefined;
+  if (!param || !Object.values(NameChangeStatus).includes(param as any)) return undefined;
   return param as NameChangeStatus;
 }
 
 export function getSearchParam(param: string | undefined | null) {
-  if (!param) return undefined;
-  if (typeof param !== "string") return undefined;
+  if (!param || typeof param !== "string") return undefined;
   return param;
+}
+
+export function getPageParam(param: string | undefined | null) {
+  if (!param || typeof param !== "string" || !Number.isInteger(Number(param)) || Number(param) < 1) {
+    return undefined;
+  }
+
+  return parseInt(param);
 }
