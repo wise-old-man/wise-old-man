712fc3d51362ffbc3d39610c9addb8c810b2a4d3
Psikoi <ruben.amendoeira@gmail.com>
2023-05-11 23:58:17 +0200

    Use own suspense boundary for names

--- app-v2/src/app/names/page.tsx
+++ app-v2/src/app/names/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { NameChange, NameChangeStatus } from "@wise-old-man/utils";
 import { Badge } from "~/components/Badge";
 import { Pagination } from "~/components/Pagination";
@@ -41,7 +42,22 @@ export function generateMetadata(props: PageProps) {
   return { title: `Name Changes (Page ${page})` };
 }
 
-export default async function NameChangesPage(props: PageProps) {
+export default async function NameChangesPageWrapper(props: PageProps) {
+  // As of Next.js 13.4.1, modifying searchParams doesn't trigger the page's file-based suspense boundary to re-fallback.
+  // So to bypass that until there's a fix, we'll make our manage our own suspense boundary with params as a unique key.
+
+  // The "dialog" search param shouldn't trigger a re-fetch
+  const key = JSON.stringify({ ...props.searchParams, dialog: undefined });
+
+  return (
+    <Suspense key={key} fallback={<LoadingState />}>
+      {/* @ts-expect-error - Server Component  */}
+      <NameChangesPage {...props} />
+    </Suspense>
+  );
+}
+
+async function NameChangesPage(props: PageProps) {
   const { searchParams } = props;
 
   const page = getPageParam(searchParams.page) || 1;
@@ -135,4 +151,36 @@ function formatDate(date: Date) {
     month: "short",
     year: "numeric",
   });
+}
+
+function LoadingState() {
+  return (
+    <ListTable>
+      {[...Array(20)].map((_, i) => (
+        <ListTableRow key={`name_change_skeleton_${i}`}>
+          <ListTableCell>
+            <div className="h-4 w-12 animate-pulse rounded-xl bg-gray-600" />
+          </ListTableCell>
+          <ListTableCell className="py-[1.1rem]">
+            <div className="h-4 w-24 animate-pulse rounded-xl bg-gray-500" />
+          </ListTableCell>
+          <ListTableCell>
+            <ArrowRightIcon className="h-4 w-4 animate-pulse text-gray-300" />
+          </ListTableCell>
+          <ListTableCell>
+            <div className="h-4 w-20 animate-pulse rounded-xl bg-gray-500" />
+          </ListTableCell>
+          <ListTableCell>
+            <div className="h-4 w-36 animate-pulse rounded-xl bg-gray-600" />
+          </ListTableCell>
+          <ListTableCell>
+            <div className="h-4 w-36 animate-pulse rounded-xl bg-gray-600" />
+          </ListTableCell>
+          <ListTableCell>
+            <div className="h-6 w-24 animate-pulse rounded-xl bg-gray-600" />
+          </ListTableCell>
+        </ListTableRow>
+      ))}
+    </ListTable>
+  );
 }
