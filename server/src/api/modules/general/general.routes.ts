53a47749870a4a1811d92bf59a14dae9e2a60122
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2023-07-30 15:49:02 +0200

    Add global `/stats` endpoint (#1195)
    * Add "stats" endpoint with table counts
    
    * Bump API
    
    * Fix tests

--- server/src/api/modules/general/general.routes.ts
+++ server/src/api/modules/general/general.routes.ts
@@ -5,5 +5,6 @@ import * as controller from './general.controller';
 const api = Router();
 
 api.post('/api-key', setupController(controller.createApiKey));
+api.get('/stats', setupController(controller.stats));
 
 export default api;
