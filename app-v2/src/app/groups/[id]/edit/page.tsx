c21047126c31884c6535bf4fe5bb984d67a8e813
Psikoi <ruben.amendoeira@gmail.com>
2023-09-11 13:54:17 +0200

    Add placeholders for missing pages

---
+++ app-v2/src/app/groups/[id]/edit/page.tsx
@@ -0,0 +1,25 @@
+import { Container } from "~/components/Container";
+
+interface PageProps {
+  params: {
+    id: number;
+  };
+}
+
+export default function EditGroupPage(props: PageProps) {
+  return (
+    <Container>
+      <h1 className="text-h1 font-bold">Edit Group</h1>
+      <p className="mt-3 text-body text-gray-200">
+        🚧 This page hasn&apos;t been implemented yet. We recommend you to use the non-beta version of
+        it.
+      </p>
+      <a
+        href={`https://wiseoldman.net/groups/${props.params.id}/edit`}
+        className="mt-5 block text-sm text-blue-400 hover:underline"
+      >
+        Go to live version
+      </a>
+    </Container>
+  );
+}
---
+++ app-v2/src/app/groups/create/page.tsx
@@ -0,0 +1,19 @@
+import { Container } from "~/components/Container";
+
+export default function CreateGroupPage() {
+  return (
+    <Container>
+      <h1 className="text-h1 font-bold">Create Group</h1>
+      <p className="mt-3 text-body text-gray-200">
+        🚧 This page hasn&apos;t been implemented yet. We recommend you to use the non-beta version of
+        it.
+      </p>
+      <a
+        href="https://wiseoldman.net/groups/create"
+        className="mt-5 block text-sm text-blue-400 hover:underline"
+      >
+        Go to live version
+      </a>
+    </Container>
+  );
+}
