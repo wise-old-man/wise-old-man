4c0f92d2bae0103f7647b61c7df14788f95deab2
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2023-09-13 13:05:09 +0200

    Add dev mode to error boundaries (#1224)

--- app-v2/src/components/ErrorState.tsx
+++ app-v2/src/components/ErrorState.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Badge } from "./Badge";
 import { Button } from "./Button";
+import { cn } from "~/utils/styling";
 
 interface ErrorStateProps {
   tag?: string;
@@ -7,6 +9,8 @@ interface ErrorStateProps {
 }
 
 export function ErrorState(props: ErrorStateProps) {
+  const [devMode, setDevMode] = useState(process.env.NODE_ENV === "development");
+
   return (
     <div className="relative flex flex-col items-center justify-center rounded-lg border border-gray-500 px-10 py-20">
       <h1 className="text-lg font-bold">An unexpected error has occurred</h1>
@@ -14,15 +18,20 @@ export function ErrorState(props: ErrorStateProps) {
       <Button size="sm" variant="blue" onClick={() => location.reload()}>
         Refresh
       </Button>
-      {process.env.NODE_ENV === "development" && (
-        <>
-          {props.tag && (
-            <Badge variant="error" className="absolute right-4 top-4">
-              {props.tag}
-            </Badge>
-          )}
-          <code className="mt-10 rounded-lg bg-red-900/40 p-5 text-sm">{String(props.error)}</code>
-        </>
+
+      {props.tag && (
+        <button
+          onClick={() => setDevMode(!devMode)}
+          className={cn(devMode ? "opacity-100" : "opacity-0")}
+        >
+          <Badge variant="error" className="absolute right-4 top-4">
+            {props.tag}
+          </Badge>
+        </button>
+      )}
+
+      {devMode && (
+        <code className="mt-10 rounded-lg bg-red-900/40 p-5 text-sm">{String(props.error)}</code>
       )}
     </div>
   );
