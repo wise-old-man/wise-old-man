9671ca76d755a0d1e0452b879db935e2487e229f
Psikoi <ruben.amendoeira@gmail.com>
2023-09-14 12:02:17 +0200

    Add missing player states and warnings

---
+++ app-v2/src/app/groups/[id]/(details)/competitions/loading.tsx
@@ -0,0 +1,36 @@
+import { ListTable, ListTableCell, ListTableRow } from "~/components/ListTable";
+
+export default function Loading() {
+  return (
+    <div>
+      <div className="mb-1 mt-8 h-3 w-32 animate-pulse rounded-full bg-gray-700" />
+      <div className="custom-scroll overflow-x-auto">
+        <ListTable className="border-spacing-y-3">
+          {[...Array(3)].map((_, i) => (
+            <ListTableRow
+              key={`competition_skeleton_${i}`}
+              className="animate-pulse"
+              style={{ opacity: 1 - i * 0.3333 }}
+            >
+              <ListTableCell className="flex items-center gap-x-3">
+                <div className="h-6 w-6 animate-pulse rounded-full bg-gray-700" />
+                <div className="flex flex-col gap-y-2 py-1">
+                  <div className="h-4 w-60 animate-pulse rounded-lg bg-gray-700" />
+                  <div className="h-3 w-28 animate-pulse rounded-lg bg-gray-700" />
+                </div>
+              </ListTableCell>
+              <ListTableCell className="w-40 pr-4">
+                <div className="h-3 w-28 animate-pulse rounded-lg bg-gray-700" />
+              </ListTableCell>
+              <ListTableCell className="w-28 pl-0">
+                <div className="flex justify-end">
+                  <div className="h-5 w-16 animate-pulse rounded-full bg-gray-700" />
+                </div>
+              </ListTableCell>
+            </ListTableRow>
+          ))}
+        </ListTable>
+      </div>
+    </div>
+  );
+}
---
+++ app-v2/src/app/groups/[id]/(details)/gained/loading.tsx
@@ -0,0 +1,19 @@
+import { ToggleTabs, ToggleTabsList, ToggleTabsTrigger } from "~/components/ToggleTabs";
+import LoadingIcon from "~/assets/loading.svg";
+
+export default function Loading() {
+  return (
+    <div>
+      <ToggleTabs value="gained">
+        <ToggleTabsList>
+          <ToggleTabsTrigger value="hiscores">Hiscores</ToggleTabsTrigger>
+          <ToggleTabsTrigger value="gained">Gained</ToggleTabsTrigger>
+          <ToggleTabsTrigger value="records">Records</ToggleTabsTrigger>
+        </ToggleTabsList>
+      </ToggleTabs>
+      <div className="mt-10 flex h-[20rem] items-center justify-center rounded-lg border border-gray-600">
+        <LoadingIcon className="h-6 w-6 animate-spin text-gray-300" />
+      </div>
+    </div>
+  );
+}
