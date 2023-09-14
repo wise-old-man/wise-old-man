196937548a9d8dda44f47268b92b9bc36bdf65a9
Psikoi <ruben.amendoeira@gmail.com>
2023-09-14 15:26:22 +0200

    Add more loading states

--- app-v2/src/app/groups/[id]/(details)/(leaderboards)/loading.tsx
+++ app-v2/src/app/groups/[id]/(details)/(leaderboards)/loading.tsx
@@ -1,12 +1,12 @@
-import LoadingIcon from "~/assets/loading.svg";
 import { GroupLeaderboardsNavigation } from "~/components/groups/GroupLeaderboardsNavigation";
+import LoadingIcon from "~/assets/loading.svg";
 
 export default function Loading() {
   return (
     <div>
       <GroupLeaderboardsNavigation />
-      <div className="mt-10 flex h-[20rem] items-center justify-center rounded-lg border border-gray-600">
-        <LoadingIcon className="h-6 w-6 animate-spin text-gray-300" />
+      <div className="mt-10 flex h-[24rem] items-center justify-center rounded-lg border border-gray-600">
+        <LoadingIcon className="h-10 w-10 animate-spin text-gray-300" />
       </div>
     </div>
   );
