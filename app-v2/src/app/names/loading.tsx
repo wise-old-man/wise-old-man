4750e7adc8dcccdf9069fc574b621cdf15c5d2d3
Psikoi <ruben.amendoeira@gmail.com>
2023-09-21 11:15:34 +0200

    Improve names index loading state

---
+++ app-v2/src/app/names/loading.tsx
@@ -0,0 +1,37 @@
+import { ListTable, ListTableRow, ListTableCell } from "~/components/ListTable";
+
+import ArrowRightIcon from "~/assets/arrow_right.svg";
+
+export default function Loading() {
+  return (
+    <div className="custom-scroll overflow-x-auto">
+      <ListTable>
+        {[...Array(20)].map((_, i) => (
+          <ListTableRow key={`name_change_skeleton_${i}`}>
+            <ListTableCell>
+              <div className="h-4 w-12 animate-pulse rounded-xl bg-gray-600" />
+            </ListTableCell>
+            <ListTableCell className="py-[1.1rem]">
+              <div className="h-[1.05rem] w-24 animate-pulse rounded-xl bg-gray-500" />
+            </ListTableCell>
+            <ListTableCell>
+              <ArrowRightIcon className="h-4 w-4 animate-pulse text-gray-300" />
+            </ListTableCell>
+            <ListTableCell>
+              <div className="h-[1.05rem] w-20 animate-pulse rounded-xl bg-gray-500" />
+            </ListTableCell>
+            <ListTableCell>
+              <div className="h-4 w-36 animate-pulse rounded-xl bg-gray-600" />
+            </ListTableCell>
+            <ListTableCell>
+              <div className="h-4 w-36 animate-pulse rounded-xl bg-gray-600" />
+            </ListTableCell>
+            <ListTableCell>
+              <div className="h-6 w-24 animate-pulse rounded-xl bg-gray-600" />
+            </ListTableCell>
+          </ListTableRow>
+        ))}
+      </ListTable>
+    </div>
+  );
+}
