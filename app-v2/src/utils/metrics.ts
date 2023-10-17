e7d0f49f3f7ca38de7cabaf1fe03636cefdf0031
Deven Caron <devencaron@gmail.com>
2023-10-17 17:39:37 -0400

    Simplify metrics returns

--- app-v2/src/utils/metrics.ts
+++ app-v2/src/utils/metrics.ts
@@ -1,19 +1,14 @@
 import { COMBAT_SKILLS, MEMBER_SKILLS, Player } from "@wise-old-man/utils";
 
 export function getBuildContextMetrics(player: Player, metrics: any[]) {
-    let filteredMetrics = metrics;
-  
     switch(player.build) {
         case 'f2p':
-        filteredMetrics = metrics.filter(item => !MEMBER_SKILLS.includes(item.metric));
-        break;
+        return metrics.filter(item => !MEMBER_SKILLS.includes(item.metric));
         case 'f2p_lvl3':
-        filteredMetrics = metrics.filter(item => ![...MEMBER_SKILLS, ...COMBAT_SKILLS].includes(item.metric));
-        break;
+        return metrics.filter(item => ![...MEMBER_SKILLS, ...COMBAT_SKILLS].includes(item.metric));
         case 'lvl3':
-        filteredMetrics = metrics.filter(item => !COMBAT_SKILLS.includes(item.metric));
-        break;
+        return metrics.filter(item => !COMBAT_SKILLS.includes(item.metric));
     }
 
-    return filteredMetrics;
+    return metrics;
 }
