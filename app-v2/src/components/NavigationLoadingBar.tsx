c7b69fcdae432c8c18edfec9a4f58de143bbbca5
Psikoi <ruben.amendoeira@gmail.com>
2023-10-31 12:44:54 +0100

    Wrap top loader in suspense

--- app-v2/src/components/NavigationLoadingBar.tsx
+++ app-v2/src/components/NavigationLoadingBar.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import { AppProgressBar as LoadingProgressBar } from "next-nprogress-bar";
 
 export function NavigationLoadingBar() {
   return (
-    <LoadingProgressBar
-      height="3px"
-      color="#bfdbfe"
-      options={{ showSpinner: false }}
-      shallowRouting
-      delay={150}
-    />
+    <Suspense>
+      <LoadingProgressBar
+        height="3px"
+        color="#bfdbfe"
+        options={{ showSpinner: false }}
+        shallowRouting
+        delay={150}
+      />
+    </Suspense>
   );
 }
