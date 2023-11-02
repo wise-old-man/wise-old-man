148f6f49328852066d41413da392c4c924ad8714
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2023-11-02 17:18:55 +0100

    Fix firefox mobile overflow issues (#1291)

--- app/src/components/Container.tsx
+++ app/src/components/Container.tsx
@@ -3,7 +3,12 @@ import { cn } from "~/utils/styling";
 
 function Container(props: PropsWithChildren & { className?: string }) {
   return (
-    <div className={cn("mx-auto w-full max-w-7xl p-6 sm:px-8 md:p-12", props.className)}>
+    <div
+      className={cn(
+        "mx-auto w-full max-w-[calc(100vw-1.5rem)] p-6 sm:px-8 md:p-12 lg:max-w-7xl",
+        props.className
+      )}
+    >
       {props.children}
     </div>
   );
