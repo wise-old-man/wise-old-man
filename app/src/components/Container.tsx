1d3d2cd4f3d8fe1361e2accbe208557aec331a0e
Psikoi <ruben.amendoeira@gmail.com>
2023-11-05 21:04:05 +0100

    Account for scrollbar in max width

--- app/src/components/Container.tsx
+++ app/src/components/Container.tsx
@@ -5,7 +5,7 @@ function Container(props: PropsWithChildren & { className?: string }) {
   return (
     <div
       className={cn(
-        "mx-auto w-full max-w-[calc(100vw-1.5rem)] p-6 sm:px-8 md:p-12 lg:max-w-7xl",
+        "mx-auto w-full max-w-[calc(100vw-1rem)] px-4 py-6 sm:p-8 md:p-12 lg:max-w-7xl",
         props.className
       )}
     >
