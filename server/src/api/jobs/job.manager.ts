de46909cdfe2102bb5480f68d5330570f79a05dc
Psikoi <ruben.amendoeira@gmail.com>
2023-11-07 17:37:30 +0100

    disable name change and flagged cron jobs

--- server/src/api/jobs/job.manager.ts
+++ server/src/api/jobs/job.manager.ts
@@ -75,15 +75,7 @@ const CRON_JOBS = [
     interval: '0 */12 * * *' // every 12 hours
   },
   {
-    type: JobType.SCHEDULE_FLAGGED_PLAYER_REVIEW,
-    interval: '0 * * * *' // every hour
-  },
-  {
     type: JobType.SCHEDULE_GROUP_SCORE_UPDATES,
-    interval: '0 8 * * *' // everyday at 8AM
-  },
-  {
-    type: JobType.SCHEDULE_NAME_CHANGE_REVIEWS,
     interval: '0 8 * * *' // everyday at 8AM
   },
   {
