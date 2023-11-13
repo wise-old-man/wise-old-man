1d2e059527ba35b801ea60bd6c8c8eb050ed44f7
Psikoi <ruben.amendoeira@gmail.com>
2023-11-13 16:09:13 +0100

    Add profile and banner images to patron groups

--- app/src/components/players/PlayerOverviewGroups.tsx
+++ app/src/components/players/PlayerOverviewGroups.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { GroupRoleProps, MembershipWithGroup } from "@wise-old-man/utils";
 import { cn } from "~/utils/styling";
@@ -49,25 +50,36 @@ function MembershipListItem(props: MembershipWithGroup) {
   const { role, group } = props;
 
   return (
-    <div className="flex flex-col gap-x-4 gap-y-1 rounded-lg border border-gray-500 bg-gray-800 px-5 py-3 shadow-sm transition-colors">
-      <Link
-        href={`/groups/${group.id}`}
-        className="flex items-center gap-x-1.5 text-base font-medium hover:underline"
-      >
-        <span className="line-clamp-1">{group.name}</span>
-        {group.verified && (
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <VerifiedIcon className="h-4 w-4" />
-            </TooltipTrigger>
-            <TooltipContent>This group is verified on our Discord server.</TooltipContent>
-          </Tooltip>
-        )}
-      </Link>
-      <span className="flex items-center gap-x-1 text-xs text-gray-200">
-        <GroupRoleIcon role={role} />
-        {GroupRoleProps[role].name}
-      </span>
+    <div className="flex items-center gap-x-3 rounded-lg border border-gray-500 bg-gray-800 px-5 py-3 shadow-sm transition-colors">
+      {group.profileImage && (
+        <Image
+          src={group.profileImage}
+          alt={`${group.name} - Profile Image`}
+          width={40}
+          height={40}
+          className="h-10 w-10 rounded-full border border-amber-300 bg-gray-800"
+        />
+      )}
+      <div className="flex flex-col gap-x-4 gap-y-1 transition-colors">
+        <Link
+          href={`/groups/${group.id}`}
+          className="flex items-center gap-x-1.5 text-base font-medium hover:underline"
+        >
+          <span className={cn("line-clamp-1", group.patron && "text-amber-300")}>{group.name}</span>
+          {group.verified && (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <VerifiedIcon className="h-4 w-4" />
+              </TooltipTrigger>
+              <TooltipContent>This group is verified on our Discord server.</TooltipContent>
+            </Tooltip>
+          )}
+        </Link>
+        <span className="flex items-center gap-x-1 text-xs text-gray-200">
+          <GroupRoleIcon role={role} />
+          {GroupRoleProps[role].name}
+        </span>
+      </div>
     </div>
   );
 }
