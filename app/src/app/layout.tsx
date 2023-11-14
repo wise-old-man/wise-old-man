24abc10d1cd301f525773a1a41886d78c1315566
Psikoi <ruben.amendoeira@gmail.com>
2023-11-14 22:43:34 +0100

    Add flagged acc message

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -9,6 +9,7 @@ import { TailwindIndicator } from "~/components/TailwindIndicator";
 import { ReactQueryProvider } from "~/components/ReactQueryProvider";
 import { NavigationLoadingBar } from "~/components/NavigationLoadingBar";
 import "../globals.css";
+import { TopBanner } from "~/components/TopBanner";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -31,6 +32,13 @@ function RootLayout(props: PropsWithChildren) {
     <html lang="en" className={inter.variable}>
       <body>
         <NavigationLoadingBar />
+        <TopBanner
+          body={
+            <>
+              {`⚠️ There's currently a issue with the Jagex hiscores due to a recently reverted banwave that is causing some players to get flagged or archived. If you're affected, try to log out in-game (to update your hiscores) and then update your profile.`}
+            </>
+          }
+        />
         <TooltipProvider delayDuration={300}>
           <ReactQueryProvider>
             <Navigation>
