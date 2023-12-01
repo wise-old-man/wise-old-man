d09f0ff8278967a8f53537365e749dd0db038b84
MikaelSvanasbakken <31248055+MikaelSvanasbakken@users.noreply.github.com>
2023-12-01 22:07:23 +0100

    username can now contain an underscore.
    Username can now contain and underscore, which also displays it on the player statistics page.

--- server/src/api/modules/players/player.utils.ts
+++ server/src/api/modules/players/player.utils.ts
@@ -87,7 +87,7 @@ function standardize(username: string): string {
 }
 
 function sanitize(username: string): string {
-  return username.replace(/[-_\s]/g, ' ').trim();
+  return username.replace(/[-\s]/g, '').trim();
 }
 
 function validateUsername(username: string): Error | null {
@@ -108,7 +108,7 @@ function validateUsername(username: string): Error | null {
   }
 
   // If has any special characters
-  if (!new RegExp(/^[a-zA-Z0-9 ]{1,12}$/).test(standardized)) {
+  if (!new RegExp(/^[a-zA-Z0-9_ ]{1,12}$/).test(standardized)) {
     return new Error('Username cannot contain any special characters.');
   }
 
