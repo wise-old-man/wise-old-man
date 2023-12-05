14e8d9fd05b43a91c4f84ecfce4aa5a5d83659e3
Psikoi <ruben.amendoeira@gmail.com>
2023-12-05 20:57:31 +0100

    Retry "connection refused" proxied requests

--- server/src/api/services/external/jagex.service.ts
+++ server/src/api/services/external/jagex.service.ts
@@ -13,15 +13,10 @@ export const OSRS_HISCORES_URLS = {
 };
 
 async function checkIsBanned(username: string) {
-  const proxy = proxiesService.getNextProxy();
   const url = `${RUNEMETRICS_URL}?user=${username}`;
 
   try {
-    const { data } = await axios({
-      url: proxy ? url.replace('https', 'http') : url,
-      proxy
-    });
-
+    const data = await fetchWithProxy(url);
     return 'error' in data && data.error === 'NOT_A_MEMBER';
   } catch (e) {
     return false;
@@ -32,14 +27,10 @@ async function checkIsBanned(username: string) {
  * Fetches the player data from the Hiscores API.
  */
 async function fetchHiscoresData(username: string, type: PlayerType = PlayerType.REGULAR): Promise<string> {
-  const proxy = proxiesService.getNextProxy();
   const url = `${OSRS_HISCORES_URLS[type]}?player=${username}`;
 
   try {
-    const { data } = await axios({
-      url: proxy ? url.replace('https', 'http') : url,
-      proxy
-    });
+    const data = await fetchWithProxy(url);
 
     if (!data || !data.length || data.includes('Unavailable') || data.includes('<')) {
       throw new ServerError('Failed to load hiscores: Jagex service is unavailable');
@@ -53,6 +44,33 @@ async function fetchHiscoresData(username: string, type: PlayerType = PlayerType
       throw new BadRequestError('Failed to load hiscores: Invalid username.');
     } else {
       throw new ServerError('Failed to load hiscores: Connection refused.');
+    }
+  }
+}
+
+async function fetchWithProxy(url: string) {
+  const MAX_RETRIES = 3;
+  let retries = 0;
+
+  while (retries < MAX_RETRIES) {
+    const proxy = proxiesService.getNextProxy();
+
+    try {
+      const { data } = await axios({
+        url: proxy ? url.replace('https', 'http') : url,
+        proxy
+      });
+
+      return data;
+    } catch (error) {
+      // If a proxy request fails with ECONNREFUSED, try again (up to 3 times)
+      // with a different proxy and wait 1000 milliseconds in between attempts.
+      if (proxy && 'code' in error && error.code === 'ECONNREFUSED') {
+        retries++;
+        await new Promise(r => setTimeout(r, 1000)); // Sleep for 1s
+      } else {
+        throw error;
+      }
     }
   }
 }
