0e7dfbed8b2aa6a9b885924700a072603db5a5fc
Patrick <sharpienero@gmail.com>
2023-12-11 20:36:11 -0500

    Simplify logic slightly

--- app/src/components/CustomPeriodDialog.tsx
+++ app/src/components/CustomPeriodDialog.tsx
@@ -29,17 +29,62 @@ export function CustomPeriodDialog(props: CustomPeriodDialogProps) {
   );
 
   const [endDate, setEndDate] = useState<DateValue>(toCalendarDate(new Date()));
-
   const [invalidStartDate, setInvalidStartDate] = useState(false);
+
   const [invalidEndDate, setInvalidEndDate] = useState(false);
+  const [startDateAfterEndDate, setStartDateAfterEndDate] = useState(false);
+
+  const earliestDate = toCalendarDate(new Date(2013, 0, 1));
+  const typeEnum = { START: 'start', END: 'end' };
+
+ function resetStates() {
+    setInvalidStartDate(false);
+    setInvalidEndDate(false);
+    setStartDateAfterEndDate(false);
+ }
+
+ function getLabel(type: string) {
+    if (type === typeEnum.START && invalidStartDate) {
+      return <Label className="mb-2 block text-xs text-red-500">Start date must be after {earliestDate.toString()}</Label>
+    } else if (type === typeEnum.END && invalidEndDate) {
+      return <Label className="mb-2 block text-xs text-red-500">End date must not be in the future</Label>
+    } else if (type === typeEnum.END && startDateAfterEndDate) {
+      return <Label className="mb-2 block text-xs text-red-500">End date must be after start date</Label>
+    } else if (type === typeEnum.END) {
+      return <Label className="mb-2 block text-xs text-gray-200">End date</Label>
+    } else {
+      return <Label className="mb-2 block text-xs text-gray-200">Start date</Label>
+    }
+ }
 
   function handleSelection() {
+    resetStates();
+
+    // Prevent users from checking metrics before 2013
     if (startDate.year < 2013) {
       setInvalidStartDate(true);
       setStartDate(toCalendarDate(new Date(2013, 0, 1)));
       return;
     }
 
+    // Prevent users from checking past metrics that don't exist
+    if (endDate.year < 2013) {
+      setInvalidEndDate(true);
+      setEndDate(toCalendarDate(new Date(2013, 0, 1)));
+      return;
+      }
+
+    // Check if endtime is before starttime
+    if (toDate(endDate, endTime) < toDate(startDate, startTime)) {
+      console.log("End date is before start date");
+      setStartDateAfterEndDate(true);
+      //Set the end date to a minute after the start date
+      setEndDate(toCalendarDate(toDate(startDate, startTime)));
+      setEndTime(new Time(toDate(startDate, startTime).getMinutes()+1));
+      return;
+    }
+
+    // Prevent users from checking future metrics that don't exist
     if (toDate(endDate, endTime) > new Date()) {
       setInvalidEndDate(true);
       setEndTime(new Time(new Date().getHours(), new Date().getMinutes()));
@@ -47,8 +92,6 @@ export function CustomPeriodDialog(props: CustomPeriodDialogProps) {
       return;
     }
 
-    setInvalidStartDate(false);
-    setInvalidEndDate(false);
     const startDateTime = toDate(startDate, startTime);
     const endDateTime = toDate(endDate, endTime);
 
@@ -79,7 +122,7 @@ export function CustomPeriodDialog(props: CustomPeriodDialogProps) {
         >
           <div className="flex grow gap-x-4">
             <div className="grow">
-              { !invalidStartDate ? <Label className="mb-2 block text-xs text-gray-200">Start date</Label> : <Label className="mb-2 block text-xs text-red-500">Start date must be after 01/01/2013</Label> }
+              { getLabel(typeEnum.START) }
               <DateTimePicker inDialog value={startDate} onChange={setStartDate} />
             </div>
             <div className="grow">
@@ -89,7 +132,7 @@ export function CustomPeriodDialog(props: CustomPeriodDialogProps) {
           </div>
           <div className="mt-5 flex grow gap-x-4">
             <div className="grow">
-              { !invalidEndDate ? <Label className="mb-2 block text-xs text-gray-200">End date</Label> : <Label className="mb-2 block text-xs text-red-500">Enter valid end date</Label> }
+              {getLabel(typeEnum.END)}
               <DateTimePicker inDialog value={endDate} onChange={setEndDate}/>
             </div>
             <div className="grow">
