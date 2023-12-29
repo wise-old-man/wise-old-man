89fe1093feac2a4308ef25db53e4dcf12ea9ccd7
Psikoi <ruben.amendoeira@gmail.com>
2023-12-30 00:16:29 +0100

    Re-use membership list item component

--- app/src/components/players/PlayerOverviewGroups.tsx
+++ app/src/components/players/PlayerOverviewGroups.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
-import { GroupRoleProps, MembershipWithGroup } from "@wise-old-man/utils";
 import { cn } from "~/utils/styling";
 import { getPlayerGroups } from "~/services/wiseoldman";
 import { Label } from "../Label";
-import { GroupRoleIcon } from "../Icon";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../Tooltip";
-
-import VerifiedIcon from "~/assets/verified.svg";
+import { MembershipListItem } from "../groups/MembershipListItem";
 
 interface PlayerOverviewMembershipsProps {
   username: string;
@@ -29,7 +24,7 @@ export async function PlayerOverviewMemberships(props: PlayerOverviewMemberships
       <Label className="text-xs leading-4 text-gray-200">Group affilitations</Label>
       <div className={cn("mt-2 flex flex-col gap-y-2", !hasMoreGroups && "pb-4")}>
         {highlighted.map((m) => (
-          <MembershipListItem {...m} key={m.group.name} />
+          <MembershipListItem {...m} key={m.group.id} />
         ))}
       </div>
       {hasMoreGroups && (
@@ -42,44 +37,6 @@ export async function PlayerOverviewMemberships(props: PlayerOverviewMemberships
           </Link>
         </div>
       )}
-    </div>
-  );
-}
-
-function MembershipListItem(props: MembershipWithGroup) {
-  const { role, group } = props;
-
-  return (
-    <div className="flex items-center gap-x-3 rounded-lg border border-gray-500 bg-gray-800 px-5 py-3 shadow-sm transition-colors">
-      {group.profileImage && (
-        <Image
-          src={group.profileImage}
-          alt={`${group.name} - Profile Image`}
-          width={40}
-          height={40}
-          className="h-10 w-10 rounded-full border border-amber-300 bg-gray-800"
-        />
-      )}
-      <div className="flex flex-col gap-x-4 gap-y-1 transition-colors">
-        <Link
-          href={`/groups/${group.id}`}
-          className="flex items-center gap-x-1.5 text-base font-medium hover:underline"
-        >
-          <span className={cn("line-clamp-1", group.patron && "text-amber-300")}>{group.name}</span>
-          {group.verified && (
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <VerifiedIcon className="h-4 w-4" />
-              </TooltipTrigger>
-              <TooltipContent>This group is verified on our Discord server.</TooltipContent>
-            </Tooltip>
-          )}
-        </Link>
-        <span className="flex items-center gap-x-1 text-xs text-gray-200">
-          <GroupRoleIcon role={role} />
-          {GroupRoleProps[role].name}
-        </span>
-      </div>
     </div>
   );
 }
