fb95411a214159fd158d0e04a3f46977de9be72e
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2024-02-07 00:45:35 +0100

    [app] Redirect all routes to maintenance page in maintenance mode (#1419)
    * Redirect all routes to maintenance page
    
    * idk

--- app/src/app/maintenance/page.tsx
+++ app/src/app/maintenance/page.tsx
@@ -1,6 +1,6 @@
 import DiscordIcon from "~/assets/discord.svg";
 
-export function MaintenanceNotice() {
+export default function MaintenancePagr() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-y-5">
       <p className="max-w-sm text-center text-lg text-gray-200">
