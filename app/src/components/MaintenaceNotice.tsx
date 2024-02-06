ab5e3cb75865eeb1dee267c8985d00a0055100a9
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2024-02-07 00:01:14 +0100

    Add app maintenance mode (#1418)

---
+++ app/src/components/MaintenaceNotice.tsx
@@ -0,0 +1,18 @@
+import DiscordIcon from "~/assets/discord.svg";
+
+export function MaintenanceNotice() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-y-5">
+      <p className="max-w-sm text-center text-lg text-gray-200">
+        Wise Old Man is currently undergoing maintenance. Please check back later.
+      </p>
+      <a
+        href="https://wiseoldman.net/discord"
+        className="flex items-center gap-x-2 text-sm hover:underline"
+      >
+        Join our Discord
+        <DiscordIcon className="h-5 w-5" />
+      </a>
+    </div>
+  );
+}
