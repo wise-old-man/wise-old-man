dd60acb44fa09c0559ca549b44200ad887dc3821
Alec <aleccaputo7@gmail.com>
2024-03-02 12:20:29 -0500

    #1459 Build both a cjs and esm bundle (#1461)
    * #1459 remove module declaration
    
    Since we are still only building a commonJS bundle and no esm bundle we should remove the module declaration so projects using esm don't blow up.
    
    Future steps:
    export both bundles
    
    * Update package.json
    
    * build both cjs and esm builds

--- client-js/rollup.config.js
+++ client-js/rollup.config.js
@@ -2,11 +2,42 @@ import dts from 'rollup-plugin-dts';
 import typescript from '@rollup/plugin-typescript';
 
 export default [
+  //commonJs
   {
     input: 'src/index.ts',
     output: {
-      dir: 'dist',
+      file: 'dist/cjs/index.cjs',
       format: 'cjs',
+      external: ['dayjs'],
+      sourcemap: false
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json'
+      })
+    ]
+  },
+  // ES Modules
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/es/index.js',
+      format: 'es',
+      external: ['dayjs'],
+      sourcemap: false
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json'
+      })
+    ]
+  },
+  // ES for browsers
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/es/index.mjs',
+      format: 'es',
       external: ['dayjs'],
       sourcemap: false
     },
