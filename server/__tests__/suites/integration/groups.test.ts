5f6167dee7b5c7352556d4334e28b269766581b6
Alec <aleccaputo7@gmail.com>
2024-04-02 08:28:23 -0400

    #1490 Add Group Role Order (table + api) (#1496)
    * #1490 wip
    
    * fix schema, write tests, rename
    
    * remove test var
    
    * add to group details
    
    * update client type
    
    * updates tests and package versions
    
    * add magic client type
    
    * remove todo
    
    * sort members by role order if it exists
    
    * sort members util
    
    * test membership ordering works
    
    * pr comments
    
    * PR comments
    
    * Update package.json
    
    * fix test
    
    * update export csv to use role order sorting

--- server/__tests__/suites/integration/groups.test.ts
+++ server/__tests__/suites/integration/groups.test.ts
@@ -1207,6 +1207,76 @@ describe('Group API', () => {
       expect(onMembersJoinedEvent).not.toHaveBeenCalled();
       expect(onMembersRolesChangedEvent).not.toHaveBeenCalled();
     });
+
+    it('should edit group role order', async () => {
+      const response = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+        verificationCode: globalData.testGroupOneLeader.verificationCode,
+        roleOrders: [
+          { role: 'air', index: 2 },
+          { role: 'member', index: 1 }
+        ]
+      });
+
+      expect(response.status).toBe(200);
+
+      const response2 = await api.get(`/groups/${globalData.testGroupOneLeader.id}`);
+
+      expect(response2.status).toBe(200);
+      expect(response2.body.roleOrders.length).toBe(2);
+      // ensure the server sorts
+      expect(response2.body.roleOrders[0]).toMatchObject({
+        role: 'member',
+        index: 1
+      });
+      expect(response2.body.roleOrders[1]).toMatchObject({
+        role: 'air',
+        index: 2
+      });
+
+      const overwriteResponse = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+        verificationCode: globalData.testGroupOneLeader.verificationCode,
+        roleOrders: [{ role: 'beast', index: 1 }]
+      });
+
+      expect(overwriteResponse.status).toBe(200);
+
+      const overwriteResponse2 = await api.get(`/groups/${globalData.testGroupOneLeader.id}`);
+      expect(overwriteResponse2.status).toBe(200);
+      expect(overwriteResponse2.body.roleOrders.length).toBe(1);
+    });
+
+    it('should not edit role order (if same index or role)', async () => {
+      const response = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+        verificationCode: globalData.testGroupOneLeader.verificationCode,
+        roleOrders: [
+          { role: 'member', index: 1 },
+          { role: 'air', index: 1 }
+        ]
+      });
+      expect(response.status).toBe(400);
+
+      const response2 = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+        verificationCode: globalData.testGroupOneLeader.verificationCode,
+        roleOrders: [
+          { role: 'air', index: 1 },
+          { role: 'air', index: 2 }
+        ]
+      });
+      expect(response2.status).toBe(400);
+    });
+  });
+
+  it('should delete role order (given empty array)', async () => {
+    const response = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+      verificationCode: globalData.testGroupOneLeader.verificationCode,
+      roleOrders: []
+    });
+    expect(response.status).toBe(200);
+
+    const overwriteResponse2 = await api.get(`/groups/${globalData.testGroupOneLeader.id}`);
+
+    expect(response.status).toBe(200);
+    expect(overwriteResponse2.body.roleOrders.length).toBe(0);
   });
 
   describe('3 - Search Groups', () => {
@@ -2070,7 +2140,7 @@ describe('Group API', () => {
       expect(response.body.memberships.length).toBe(0);
     });
 
-    it('should view details', async () => {
+    it('should view details (no role ordering)', async () => {
       const response = await api.get(`/groups/${globalData.testGroupOneLeader.id}`);
 
       expect(response.status).toBe(200);
@@ -2114,6 +2184,63 @@ describe('Group API', () => {
     });
   });
 
+  it('should view details (with role ordering)', async () => {
+    // set order
+    const roleOrderResponse = await api.put(`/groups/${globalData.testGroupOneLeader.id}`).send({
+      verificationCode: globalData.testGroupOneLeader.verificationCode,
+      roleOrders: [
+        { role: 'leader', index: 1 },
+        { role: 'artisan', index: 2 },
+        { role: 'member', index: 3 },
+        { role: 'firemaker', index: 4 },
+        { role: 'achiever', index: 5 }
+      ]
+    });
+
+    expect(roleOrderResponse.status).toBe(200);
+
+    const response = await api.get(`/groups/${globalData.testGroupOneLeader.id}`);
+
+    expect(response.status).toBe(200);
+
+    expect(response.body.verificationHash).not.toBeDefined();
+
+    expect(response.body).toMatchObject({
+      clanChat: 'wiseoldman',
+      description: 'when I was a young boy, my father took me into the city to see a marching band',
+      homeworld: 302,
+      memberCount: 5
+    });
+
+    expect(response.body.memberships.length).toBe(5);
+
+    // this order is determined by the roleOrder defined on the group
+    expect(response.body.memberships[0]).toMatchObject({
+      role: 'leader',
+      player: { username: 'alexsuperfly' }
+    });
+
+    expect(response.body.memberships[1]).toMatchObject({
+      role: 'artisan',
+      player: { username: 'swampletics' }
+    });
+
+    expect(response.body.memberships[2]).toMatchObject({
+      role: 'member',
+      player: { username: 'rorro' }
+    });
+
+    expect(response.body.memberships[3]).toMatchObject({
+      role: 'firemaker',
+      player: { username: 'zezima' }
+    });
+
+    expect(response.body.memberships[4]).toMatchObject({
+      role: 'achiever',
+      player: { username: 'psikoi' }
+    });
+  });
+
   describe('10 - View Members CSV', () => {
     it('should not view members CSV (group not found)', async () => {
       const response = await api.get('/groups/9999/csv');
@@ -2151,10 +2278,10 @@ describe('Group API', () => {
 
       // Check the table body
       expect(rows[1]).toMatch('alexsuperfly,leader,');
-      expect(rows[2]).toMatch('psikoi,achiever');
-      expect(rows[3]).toMatch('swampletics,artisan');
+      expect(rows[2]).toMatch('swampletics,artisan');
+      expect(rows[3]).toMatch('rorro,member');
       expect(rows[4]).toMatch('zezima,firemaker');
-      expect(rows[5]).toMatch('rorro,member');
+      expect(rows[5]).toMatch('psikoi,achiever');
     });
   });
 
