efed81979ac62d45abed083d2f563944b1f1ce05
autumn-smellegy <120493279+autumn-smellegy@users.noreply.github.com>
2024-06-25 14:10:31 -0400

    Update f2p-im pray bxp ratios at 99 atk str def too

--- server/src/api/modules/efficiency/configs/ehp/f2p_ironman.ehp.ts
+++ server/src/api/modules/efficiency/configs/ehp/f2p_ironman.ehp.ts
@@ -113,7 +113,7 @@ export default [
         startExp: 13_034_431,
         endExp: 200_000_000,
         end: true,
-        ratio: 0.1119
+        ratio: 0.112
       },
       {
         originSkill: Skill.ATTACK,
@@ -167,7 +167,7 @@ export default [
         startExp: 37_224,
         endExp: 200_000_000,
         end: true,
-        ratio: 0.1126
+        ratio: 0.1127
       },
       {
         originSkill: Skill.DEFENCE,
@@ -325,7 +325,7 @@ export default [
         startExp: 13_034_431,
         endExp: 200_000_000,
         end: true,
-        ratio: 0.1117
+        ratio: 0.1118
       },
       {
         originSkill: Skill.STRENGTH,
