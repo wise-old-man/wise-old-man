1150f3869ce1fc428556d52537f4911129f8db7a
Psikoi <ruben.amendoeira@gmail.com>
2024-08-22 22:13:18 +0200

    Retire hidden_x_created discord events

--- server/src/api/services/external/discord.service.ts
+++ server/src/api/services/external/discord.service.ts
@@ -1,15 +1,9 @@
 import axios from 'axios';
 import { WebhookClient } from 'discord.js';
 import prisma, { Achievement, Competition, Player } from '../../../prisma';
-import {
-  FlaggedPlayerReviewContext,
-  GroupListItem,
-  MemberJoinedEvent,
-  MemberRoleChangeEvent
-} from '../../../utils';
+import { FlaggedPlayerReviewContext, MemberJoinedEvent, MemberRoleChangeEvent } from '../../../utils';
 import {
   CompetitionDetails,
-  CompetitionListItem,
   CompetitionWithParticipations
 } from '../../modules/competitions/competition.types';
 import logger from '../../util/logging';
@@ -160,18 +154,6 @@ async function dispatchMembersRolesChanged(events: MemberRoleChangeEvent[]) {
   });
 }
 
-async function dispatchHiddenGroupCreated(group: GroupListItem) {
-  dispatch('HIDDEN_GROUP_CREATED', {
-    group
-  });
-}
-
-async function dispatchHiddenCompetitionCreated(competition: CompetitionListItem) {
-  dispatch('HIDDEN_COMPETITION_CREATED', {
-    competition
-  });
-}
-
 /**
  * Select all new group members and dispatch them to our discord API,
  * so that it can notify any relevant guilds/servers.
@@ -290,8 +272,6 @@ export {
   dispatchMembersRolesChanged,
   dispatchNameChanged,
   dispatchPlayerFlaggedReview,
-  dispatchHiddenGroupCreated,
-  dispatchHiddenCompetitionCreated,
   sendMonitoringMessage,
   sendPatreonUpdateMessage
 };
