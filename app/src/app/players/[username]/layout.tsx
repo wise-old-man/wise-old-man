9654d7608b5cb730261534d26dd0f0e93513c880
Stephan Bijzitter <StephanBijzitter@gmail.com>
2024-10-01 14:44:40 +0200

    Fix a grammatical mistake and a punctuation issue (#1591)

--- app/src/app/players/[username]/layout.tsx
+++ app/src/app/players/[username]/layout.tsx
@@ -203,7 +203,7 @@ function PlayerStatusAlert(props: { player: PlayerDetails }) {
           <AlertTitle>This player is banned</AlertTitle>
           <AlertDescription>
             This player could not be found on the hiscores last time we checked. It was then confirmed to
-            be banned via RuneMetrics. You can update to re-check this status, it may take a few minutes
+            be banned via RuneMetrics. You can update to re-check this status; it may take a few minutes
             to complete.
           </AlertDescription>
         </div>
@@ -231,7 +231,7 @@ function PlayerStatusAlert(props: { player: PlayerDetails }) {
             {` for more information.`}
 
             <p className="mt-5">
-              <span className="text-white">Note (October 1st):</span> There&apos;s currently a issue
+              <span className="text-white">Note (October 1st):</span> There&apos;s currently an issue
               with the Jagex hiscores due to a recent rollback that is causing some players to
               get flagged. If you&apos;re affected, try to log out in-game (to update your
               hiscores) and then update your WOM profile.
