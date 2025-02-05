5a8c6e7e2da5109d5b03acde7d89dc21b189b3cd
Robin <5983417+rorro@users.noreply.github.com>
2025-02-05 12:59:08 +0100

    fix name in snapshot

--- server/prisma/migrations/20250205115734_add_the_royal_titans_boss/migration.sql
+++ server/prisma/migrations/20250205115734_add_the_royal_titans_boss/migration.sql
@@ -5,5 +5,5 @@ ALTER TYPE "metric" ADD VALUE 'the_royal_titans';
 ALTER TABLE "deltas" ADD COLUMN     "the_royal_titans" INTEGER NOT NULL DEFAULT 0;
 
 -- AlterTable
-ALTER TABLE "snapshots" ADD COLUMN     "the_royalTitansKills" INTEGER NOT NULL DEFAULT -1,
+ALTER TABLE "snapshots" ADD COLUMN     "the_royal_titansKills" INTEGER NOT NULL DEFAULT -1,
 ADD COLUMN     "the_royal_titansRank" INTEGER NOT NULL DEFAULT -1;
