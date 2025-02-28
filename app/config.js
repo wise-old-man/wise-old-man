aadbf23d8694658c164aa3b7622bd44fce8a15b0
Robin <5983417+rorro@users.noreply.github.com>
2025-02-28 18:39:21 +0100

    Disable hiscores broken warning banner

--- app/config.js
+++ app/config.js
@@ -4,9 +4,9 @@ const MAINTENANCE_MODE = {
 };
 
 const ANNOUNCEMENT_BANNER = {
-  enabled: true,
-  color: "yellow", // "blue" / "yellow" / undefined
-  message: `The OSRS hiscores have been broken for the past few days. Some auto-update group/competition features may not work as expected, we recommend players update themselves individually for now.`,
+  enabled: false,
+  color: undefined, // "blue" / "yellow" / undefined
+  message: undefined,
 };
 
 module.exports = {
