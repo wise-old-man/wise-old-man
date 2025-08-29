872866267172cad5fee1b1b45ac8d88f97397f5b
Psikoi <ruben.amendoeira@gmail.com>
2025-08-29 16:35:04 +0200

    Add dangling job purging cron job

--- server/src/jobs/types/job-type.enum.ts
+++ server/src/jobs/types/job-type.enum.ts
@@ -22,6 +22,7 @@ export enum JobType {
   DISPATCH_MEMBERS_ROLES_CHANGED_DISCORD_EVENT = 'dispatch-members-roles-changed-discord-event',
   DISPATCH_PLAYER_FLAGGED_DISCORD_EVENT = 'dispatch-player-flagged-discord-event',
   INVALIDATE_DELTAS = 'invalidate-deltas',
+  PURGE_DANGLING_JOBS = 'purge-dangling-jobs',
   RECALCULATE_PLAYER_ACHIEVEMENTS = 'recalculate-player-achievements',
   REMOVE_PLAYERS_FROM_GROUP_COMPETITIONS = 'remove-players-from-group-competitions',
   REVIEW_NAME_CHANGE = 'review-name-change',
