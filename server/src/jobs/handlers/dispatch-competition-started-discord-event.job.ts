274eec0bec39a9bc75a619f056f1e83941ce7cda
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-09-01 00:13:29 +0200

    [api] Fix comp discord bot events payload (#1774)
    * Fix competition status discord bot event payload
    
    * bump api

--- server/src/jobs/handlers/dispatch-competition-started-discord-event.job.ts
+++ server/src/jobs/handlers/dispatch-competition-started-discord-event.job.ts
@@ -1,4 +1,5 @@
 import { isErrored } from '@attio/fetchable';
+import { formatCompetitionResponse } from '../../api/responses';
 import prisma from '../../prisma';
 import { DiscordBotEventType, dispatchDiscordBotEvent } from '../../services/discord.service';
 import { Job } from '../job.class';
@@ -25,6 +26,30 @@ export class DispatchCompetitionStartedDiscordEventJob extends Job<Payload> {
     const competition = await prisma.competition.findFirst({
       where: {
         id: payload.competitionId
+      },
+      include: {
+        _count: {
+          select: {
+            participations: true
+          }
+        },
+        group: {
+          include: {
+            _count: {
+              select: {
+                memberships: true
+              }
+            }
+          }
+        },
+        metrics: {
+          where: {
+            deletedAt: null
+          },
+          orderBy: {
+            createdAt: 'asc'
+          }
+        }
       }
     });
 
@@ -32,9 +57,22 @@ export class DispatchCompetitionStartedDiscordEventJob extends Job<Payload> {
       return;
     }
 
+    const competitionResponse = formatCompetitionResponse(
+      {
+        ...competition,
+        participantCount: competition._count.participations
+      },
+      competition.group === null
+        ? null
+        : {
+            ...competition.group,
+            memberCount: competition.group?._count.memberships
+          }
+    );
+
     const dispatchResult = await dispatchDiscordBotEvent(DiscordBotEventType.COMPETITION_STARTED, {
       groupId: competition.groupId,
-      competition
+      competition: competitionResponse
     });
 
     if (isErrored(dispatchResult) && dispatchResult.error.code === 'FAILED_TO_SEND_DISCORD_BOT_EVENT') {
