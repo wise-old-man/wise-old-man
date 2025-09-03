9f6bd77fc93f74f52b29a2d39f87f6d3a2a58914
Psikoi <ruben.amendoeira@gmail.com>
2025-09-03 18:11:47 +0100

    Remove basic auth for bull board deployment

--- server/src/bull-board.ts
+++ server/src/bull-board.ts
@@ -2,7 +2,6 @@ import { createBullBoard } from '@bull-board/api';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 import express from 'express';
-import basicAuth from 'express-basic-auth';
 import { jobManager } from './jobs';
 import logger from './services/logging.service';
 import { redisClient } from './services/redis.service';
@@ -20,19 +19,14 @@ handleServerInit(async () => {
   const expressApp = express();
 
   const serverAdapter = new ExpressAdapter();
-  serverAdapter.setBasePath("/");
+  serverAdapter.setBasePath('/');
 
   createBullBoard({
     queues: jobManager.getQueues().map(q => new BullMQAdapter(q)),
     serverAdapter
   });
 
-  const authMiddleware = basicAuth({
-    users: { admin: process.env.ADMIN_PASSWORD },
-    challenge: true
-  });
-
-  expressApp.use("/", authMiddleware, serverAdapter.getRouter());
+  expressApp.use('/', serverAdapter.getRouter());
 
   const server = expressApp.listen(BULL_BOARD_PORT, () => {
     const version = process.env.npm_package_version;
