a732527e4094bc778bd8b9ce75fc360fc6f6355a
Rafael Santos <rafael@risantos.com>
2025-09-13 10:43:47 +0100

    chore(app): update eslint (#1786)
    - Update `eslint` to `~9.35.0`
    - Update `eslint-config-next` to `^15.5.3`
    - Update `prettier` to `^3.6.2`
    - Update `prettier-plugin-tailwindcss` to `^0.6.14`
    - Replace deprecated @typescript-eslint/eslint-plugin and @typescript-eslint/parser with typescript-eslint package
    - Migrate ESLint configuration to eslint.config.mjs using the new flat config format

--- app/src/components/players/PlayerGainedBarchart.tsx
+++ app/src/components/players/PlayerGainedBarchart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
 import dynamicImport from "next/dynamic";
--- app/src/components/players/PlayerGainedChart.tsx
+++ app/src/components/players/PlayerGainedChart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
