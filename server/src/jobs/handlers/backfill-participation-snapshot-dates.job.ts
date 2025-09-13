2c89a2d757698aef112a6345b7108c4e75791214
Psikoi <ruben.amendoeira@gmail.com>
2025-09-14 01:24:05 +0200

    Add end snapshot backfill

--- server/src/jobs/handlers/backfill-participation-snapshot-dates.job.ts
+++ server/src/jobs/handlers/backfill-participation-snapshot-dates.job.ts
@@ -26,6 +26,22 @@ export class BackfillParticipationSnapshotDatesJob extends Job<unknown> {
       AND pp."playerId" = to_update."playerId"
     `;
 
+    await prisma.$executeRaw`
+      WITH "to_update" AS (
+        SELECT pp."competitionId", pp."playerId", s."createdAt"
+        FROM "participations" pp
+        JOIN "snapshots" s ON pp."endSnapshotId" = s."id"
+        WHERE pp."endSnapshotDate" IS NULL
+          AND pp."endSnapshotId" IS NOT NULL
+        LIMIT 100
+      )
+      UPDATE "participations" pp
+      SET "endSnapshotDate" = to_update."createdAt"
+      FROM to_update
+      WHERE pp."competitionId" = to_update."competitionId"
+      AND pp."playerId" = to_update."playerId"
+    `;
+
     this.jobManager.add(JobType.BACKFILL_PARTICIPATION_SNAPSHOT_DATES, {});
   }
 }
