d8a0e1858c6a513b078f11e8980d79daca5333a4
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-10-02 23:38:05 +0200

    Finish backfill implementation

--- server/src/jobs/handlers/backfill-delete-duplicate-snapshots.job.ts
+++ server/src/jobs/handlers/backfill-delete-duplicate-snapshots.job.ts
@@ -2,6 +2,8 @@ import prisma from '../../prisma';
 import logger from '../../services/logging.service';
 import { Job } from '../job.class';
 import { JobOptions } from '../types/job-options.type';
+import { JobPriority } from '../types/job-priority.enum';
+import { JobType } from '../types/job-type.enum';
 
 interface Payload {
   playerId: number;
@@ -44,10 +46,64 @@ export class BackfillDeleteDuplicateSnapshotsJob extends Job<Payload> {
       return ids.slice(1);
     });
 
+    const result = await prisma.$transaction(async transaction => {
+      // Unset player snapshot fks
+      const updatedPlayer = await transaction.player.update({
+        where: {
+          id: payload.playerId
+        },
+        data: {
+          latestSnapshotDate: null,
+          latestSnapshotId: null
+        }
+      });
+
+      // Unset participation fks
+      const updatedParticipationsResult = await transaction.participation.updateMany({
+        where: {
+          playerId: payload.playerId
+        },
+        data: {
+          startSnapshotId: null,
+          startSnapshotDate: null,
+          endSnapshotId: null,
+          endSnapshotDate: null
+        }
+      });
+
+      const deletedSnapshotsResult = await transaction.snapshot.deleteMany({
+        where: {
+          id: { in: idsToDelete }
+        }
+      });
+
+      return {
+        updatedPlayer,
+        updatedParticipationsResult,
+        deletedSnapshotsResult
+      };
+    });
+
+    const { updatedPlayer, updatedParticipationsResult, deletedSnapshotsResult } = result;
+
     logger.debug('Backfilling duplicate snapshots for player', {
       playerId: payload.playerId,
+      username: updatedPlayer.username,
       idsToDelete,
-      map: Array.from(map.entries())
+      deletedSnapshotsCount: deletedSnapshotsResult.count,
+      updatedParticipationsCount: updatedParticipationsResult.count
     });
+
+    this.jobManager.add(
+      JobType.SYNC_PLAYER_COMPETITION_PARTICIPATIONS,
+      { username: updatedPlayer.username, forceRecalculate: true },
+      { priority: JobPriority.HIGH }
+    );
+
+    this.jobManager.add(
+      JobType.UPDATE_PLAYER,
+      { username: updatedPlayer.username },
+      { priority: JobPriority.HIGH }
+    );
   }
 }
