89740d59845064e27be2b4cc075591c43fa9a5ed
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-10-02 23:50:11 +0200

    Remove backfill fanout limit

--- server/src/jobs/handlers/backfill-delete-duplicate-snapshots-fanout.job.ts
+++ server/src/jobs/handlers/backfill-delete-duplicate-snapshots-fanout.job.ts
@@ -5,9 +5,8 @@ import { JobType } from '../types/job-type.enum';
 
 export class BackfillDeleteDuplicateSnapshotsFanoutJob extends Job<unknown> {
   async execute() {
-    // Limit to just 20 results for now, as a test
     const playerIds = await prisma.$queryRaw<{ playerId: number }[]>`
-      SELECT distinct("playerId") FROM public.failed_pairs_2 LIMIT 20;
+      SELECT distinct("playerId") FROM public.failed_pairs_2;
     `;
 
     for (const { playerId } of playerIds) {
