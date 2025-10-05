40e9410ad6d016862f915e081b3854f816a496c9
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-10-05 16:57:01 +0200

    [api] Add bulljob to the job class + drain queue (#1800)
    * Add bulljob to the job class + drain queue
    
    * Make bulljob optional

--- server/src/jobs/job.class.ts
+++ server/src/jobs/job.class.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { Job as BullJob } from 'bullmq';
 import type { JobManager } from './job-manager';
 import type { JobOptions } from './types/job-options.type';
 
@@ -6,9 +7,14 @@ export class Job<T> {
   public static options: JobOptions = {};
 
   public jobManager: JobManager;
+  public bullJob: BullJob | undefined;
 
-  constructor(jobManager: JobManager) {
+  constructor(jobManager: JobManager, bulljob?: BullJob) {
     this.jobManager = jobManager;
+
+    if (bulljob) {
+      this.bullJob = bulljob;
+    }
   }
 
   async execute(payload: T): Promise<void> {}
