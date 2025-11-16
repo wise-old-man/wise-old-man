92a15c0ab8de2a0d1665ac9ce41b32318cf183a9
Psikoi <ruben.amendoeira@gmail.com>
2025-11-16 14:40:23 +0100

    Add if not exists to index creation

--- server/prisma/migrations/20251113083841_add_sailing_cols_to_player/migration.sql
+++ server/prisma/migrations/20251113083841_add_sailing_cols_to_player/migration.sql
@@ -3,4 +3,4 @@ ALTER TABLE "public"."players" ADD COLUMN     "sailing" INTEGER NOT NULL DEFAULT
 ADD COLUMN     "sailingRank" INTEGER NOT NULL DEFAULT -1;
 
 -- CreateIndex
-CREATE INDEX "players_updated_at_sailing_sailing_rank" ON "public"."players"("updatedAt", "sailing", "sailingRank");
+CREATE INDEX IF NOT EXISTS "players_updated_at_sailing_sailing_rank" ON "public"."players"("updatedAt", "sailing", "sailingRank");
