2c388a0a751c7b761deff70d659b990f26a52322
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-12-26 15:48:50 +0100

    Add optional "username" param to the flagged player review job

--- server/src/jobs/handlers/schedule-flagged-player-review.job.ts
+++ server/src/jobs/handlers/schedule-flagged-player-review.job.ts
@@ -1,23 +1,23 @@
+import { standardize } from '../../api/modules/players/player.utils';
 import { updatePlayer } from '../../api/modules/players/services/UpdatePlayerService';
 import prisma from '../../prisma';
 import { Player, PlayerStatus } from '../../types';
 import { Job } from '../job.class';
 
-export class ScheduleFlaggedPlayerReviewJob extends Job<unknown> {
-  async execute() {
-    // Find a flagged player
-    const results = await prisma.$queryRaw<Array<Pick<Player, 'id' | 'username'>>>`
-      SELECT "id", "username" FROM public.players
-      WHERE "status" = ${PlayerStatus.FLAGGED}::player_status
-      ORDER BY RANDOM()
-      LIMIT 1
-    `;
+interface Payload {
+  username?: string;
+}
 
-    if (results.length === 0) {
+export class ScheduleFlaggedPlayerReviewJob extends Job<Payload> {
+  async execute(payload: Payload) {
+    const flaggedPlayer =
+      payload.username === undefined
+        ? await findRandomFlaggedPlayer()
+        : await prisma.player.findFirst({ where: { username: standardize(payload.username) } });
+
+    if (flaggedPlayer === null) {
       return;
     }
-
-    const [flaggedPlayer] = results;
 
     // Force-unflag them
     await prisma.player.update({
@@ -33,4 +33,19 @@ export class ScheduleFlaggedPlayerReviewJob extends Job<unknown> {
     // or send a review message to our Discord server.
     await updatePlayer(flaggedPlayer.username);
   }
+}
+
+async function findRandomFlaggedPlayer() {
+  const results = await prisma.$queryRaw<Array<Pick<Player, 'id' | 'username'>>>`
+      SELECT "id", "username" FROM public.players
+      WHERE "status" = ${PlayerStatus.FLAGGED}::player_status
+      ORDER BY RANDOM()
+      LIMIT 1
+    `;
+
+  if (results.length === 0) {
+    return null;
+  }
+
+  return results[0];
 }
