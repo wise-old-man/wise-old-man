d4c597cde0fd9d090e32863b8749af9f1a97e4f8
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-12-26 20:08:51 +0100

    Update CI trigger comment in index.ts

--- client-js/src/index.ts
+++ client-js/src/index.ts
@@ -10,6 +10,6 @@ export * from './clients/NameChangesClient';
 export * from './clients/PlayersClient';
 export * from './clients/RecordsClient';
 
-// CI trigger 3
+// CI trigger 4
 
 export { WOMClient };
