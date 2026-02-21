303a020f0c896ac39d0405cc47a3b178013c6565
rodas96 <rodrigomiguelmorais@gmail.com>
2026-02-21 16:38:27 +0100

    Throw forbiddenerror when trying to find player achievements when player is opted out

--- server/src/api/modules/achievements/services/FindPlayerAchievementsService.ts
+++ server/src/api/modules/achievements/services/FindPlayerAchievementsService.ts
@@ -1,26 +1,28 @@
 import prisma from '../../../../prisma';
-import { Achievement } from '../../../../types';
-import { NotFoundError } from '../../../errors';
+import { Achievement, PlayerAnnotationType } from '../../../../types';
+import { NotFoundError, ForbiddenError } from '../../../errors';
 import { standardizeUsername } from '../../players/player.utils';
 
 async function findPlayerAchievements(username: string): Promise<Achievement[]> {
-  const achievements = await prisma.achievement.findMany({
-    where: {
-      player: {
-        username: standardizeUsername(username)
-      }
-    }
+  const player = await prisma.player.findFirst({
+    where: { username: standardizeUsername(username) },
+    include: { annotations: true }
   });
 
-  if (achievements.length === 0) {
-    const player = await prisma.player.findFirst({
-      where: { username: standardizeUsername(username) }
-    });
-
-    if (!player) {
-      throw new NotFoundError('Player not found.');
-    }
+  // TODO: refactor error handling
+  if (!player) {
+    throw new NotFoundError('Player not found.');
   }
+
+  if (player.annotations.some(a => a.type === PlayerAnnotationType.OPT_OUT)) {
+    throw new ForbiddenError('Player has opted out.');
+  }
+
+  const achievements = await prisma.achievement.findMany({
+    where: {
+      playerId: player.id
+    }
+  });
 
   return achievements;
 }
