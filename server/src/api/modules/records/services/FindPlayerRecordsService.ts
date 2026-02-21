be48352f9c8741cbeadf0fdd04a507b04d0e8363
rodas96 <rodrigomiguelmorais@gmail.com>
2026-02-21 17:26:49 +0100

    throw forbidden when getting player records if opted out

--- server/src/api/modules/records/services/FindPlayerRecordsService.ts
+++ server/src/api/modules/records/services/FindPlayerRecordsService.ts
@@ -1,32 +1,31 @@
-import prisma, { PrismaTypes } from '../../../../prisma';
-import { Metric, Period, Record } from '../../../../types';
-import { NotFoundError } from '../../../errors';
+import prisma from '../../../../prisma';
+import { Metric, Period, Record, PlayerAnnotationType } from '../../../../types';
+import { NotFoundError, ForbiddenError } from '../../../errors';
 import { standardizeUsername } from '../../players/player.utils';
 
 async function findPlayerRecords(username: string, period?: Period, metric?: Metric): Promise<Record[]> {
-  const query: PrismaTypes.RecordWhereInput = {
-    player: {
-      username: standardizeUsername(username)
-    }
-  };
-
-  if (period) query.period = period;
-  if (metric) query.metric = metric;
-
-  const records = await prisma.record.findMany({
-    where: { ...query },
-    orderBy: { updatedAt: 'desc' }
+  const player = await prisma.player.findFirst({
+    where: { username: standardizeUsername(username) },
+    include: { annotations: true }
   });
 
-  if (records.length === 0) {
-    const player = await prisma.player.findFirst({
-      where: { username: standardizeUsername(username) }
-    });
-
-    if (!player) {
-      throw new NotFoundError('Player not found.');
-    }
+  // TODO: refactor error handling
+  if (!player) {
+    throw new NotFoundError('Player not found.');
   }
+
+  if (player.annotations.some(a => a.type === PlayerAnnotationType.OPT_OUT)) {
+    throw new ForbiddenError('Player has opted out.');
+  }
+
+  const records = await prisma.record.findMany({
+    where: {
+      playerId: player.id,
+      ...(period ? { period } : {}),
+      ...(metric ? { metric } : {})
+    },
+    orderBy: { updatedAt: 'desc' }
+  });
 
   return records;
 }
