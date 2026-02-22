e44dfb6e88e0b1a676284c8dec3d5aef14813d78
rodas96 <rodrigomiguelmorais@gmail.com>
2026-02-22 13:42:03 +0100

    filter opt out players

--- server/src/api/modules/players/services/SearchPlayersService.ts
+++ server/src/api/modules/players/services/SearchPlayersService.ts
@@ -1,13 +1,16 @@
 import prisma from '../../../../prisma';
 import { Player, PlayerAnnotationType } from '../../../../types';
-import { optOutFilter } from '../../../../utils/shared/player-annotation.utils';
 import { PaginationOptions } from '../../../util/validation';
 
 async function searchPlayers(username: string, pagination: PaginationOptions): Promise<Player[]> {
   const players = await prisma.player.findMany({
     where: {
       username: { startsWith: username.trim(), mode: 'insensitive' },
-      ...optOutFilter(PlayerAnnotationType.OPT_OUT)
+      annotations: {
+        none: {
+          type: PlayerAnnotationType.OPT_OUT
+        }
+      }
     },
     orderBy: {
       ehp: 'desc'
