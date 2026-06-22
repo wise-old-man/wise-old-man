051d28802d0407776f814e645d12031c160288c9
Trentin Thomas <trentinthomas94@gmail.com>
2026-06-21 19:59:02 -0500

    Removed null typing

--- server/src/types/group-member-input.type.ts
+++ server/src/types/group-member-input.type.ts
@@ -3,5 +3,5 @@ import { GroupRole } from './group-role.enum';
 export type GroupMemberInput = {
   username: string;
   role: GroupRole;
-  clientSyncJoinedAt?: Date | null;
+  clientSyncJoinedAt?: Date;
 };
