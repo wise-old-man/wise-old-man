153392fd0fe4121e01b628359876a323b05c21e8
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2026-07-04 14:02:26 +0200

    [server/docs/client-js] Add bulk group deltas endpoint (#1971)
    * Add group bulk deltas endpoint
    
    * Remove server side pagination from group deltas table
    
    * Add docs and sdk method
    
    * bump server version
    
    * bump client-js again

--- app/src/app/groups/[id]/(details)/(leaderboards)/gained/page.tsx
+++ app/src/app/groups/[id]/(details)/(leaderboards)/gained/page.tsx
@@ -1,10 +1,9 @@
 import { Metric } from "@wise-old-man/utils";
-import { Pagination } from "~/components/Pagination";
 import { GroupGainedCustomPeriodDialog } from "~/components/groups/GroupGainedCustomPeriodDialog";
 import { GroupGainedTable } from "~/components/groups/GroupGainedTable";
 import { GroupLeaderboardsNavigation } from "~/components/groups/GroupLeaderboardsNavigation";
 import { getGroupDetails, getGroupGainsByDates, getGroupGainsByPeriod } from "~/services/wiseoldman";
-import { getMetricParam, getPageParam, getTimeRangeFilterParams } from "~/utils/params";
+import { getMetricParam, getTimeRangeFilterParams } from "~/utils/params";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -14,7 +13,6 @@ interface PageProps {
     id: number;
   };
   searchParams: {
-    page?: string;
     metric?: string;
     period?: string;
   };
@@ -35,30 +33,14 @@ export default async function GroupGainedPage(props: PageProps) {
   const { id } = props.params;
   const { searchParams } = props;
 
-  const page = getPageParam(searchParams.page) || 1;
   const metric = getMetricParam(searchParams.metric) || Metric.OVERALL;
   const timeRange = getTimeRangeFilterParams(new URLSearchParams(searchParams));
-
-  const RESULTS_PER_PAGE = 20;
 
   const [group, gains] = await Promise.all([
     getGroupDetails(id),
     "period" in timeRange
-      ? getGroupGainsByPeriod(
-          id,
-          metric,
-          timeRange.period,
-          RESULTS_PER_PAGE,
-          (page - 1) * RESULTS_PER_PAGE,
-        )
-      : getGroupGainsByDates(
-          id,
-          metric,
-          timeRange.startDate,
-          timeRange.endDate,
-          RESULTS_PER_PAGE,
-          (page - 1) * RESULTS_PER_PAGE,
-        ),
+      ? getGroupGainsByPeriod(id, metric, timeRange.period)
+      : getGroupGainsByDates(id, metric, timeRange.startDate, timeRange.endDate),
   ]);
 
   return (
@@ -66,9 +48,6 @@ export default async function GroupGainedPage(props: PageProps) {
       <GroupLeaderboardsNavigation />
       <div className="mt-7">
         <GroupGainedTable group={group} metric={metric} timeRange={timeRange} gains={gains} />
-        <div className="mt-4">
-          <Pagination currentPage={page} hasMorePages={gains.length >= RESULTS_PER_PAGE} />
-        </div>
       </div>
       <GroupGainedCustomPeriodDialog groupId={id} />
     </>
