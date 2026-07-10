e21e119c1e66039767cbc52e8a2d5f1eb0fdb40c
Robin <5983417+rorro@users.noreply.github.com>
2026-07-11 01:15:02 +0200

    A sort fell off

--- server/src/api/modules/players/services/FindPlayerArchivesService.ts
+++ server/src/api/modules/players/services/FindPlayerArchivesService.ts
@@ -28,6 +28,9 @@ export async function findPlayerArchives(
     where: {
       previousUsername: standardizeUsername(username),
       restoredAt: null
+    },
+    orderBy: {
+      createdAt: 'desc'
     }
   });
 
