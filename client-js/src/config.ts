d4d7377ba2e38132d297bb719c7f3ff5560d7c2a
BeeHiveJava <BeeHiveJava@users.noreply.github.com>
2026-07-24 14:48:23 +0000

    feat: make default user agent for client-js a lazily evaluated function

--- client-js/src/config.ts
+++ client-js/src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  defaultUserAgent: `WiseOldMan JS Client v${process.env.npm_package_version}`,
+  defaultUserAgent: () => `WiseOldMan JS Client v${process.env.npm_package_version}`,
   baseAPIUrl: 'https://api.wiseoldman.net/v2'
 };
